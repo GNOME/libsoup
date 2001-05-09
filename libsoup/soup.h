57a2b280e7fe967ae4b7cb56dbbe7f90865ae2a2
Rodrigo Moya <rodrigo@ximian.com>
2001-05-09 09:34:28 +0000

    added a basic SOAP messages parser, which easily lets access to the
    2001-05-09  Rodrigo Moya <rodrigo@ximian.com>
    
    	* src/soup-core/soup-parser.[ch]: added a basic SOAP messages
    	parser, which easily lets access to the message parameters
    	and faults
    
    2001-05-05  Rodrigo Moya <rodrigo@ximian.com>
    
    	* src/soup-wsdl-runtime/wsdl-soap-fault.[ch]: added to manage/retrieve
    	info from SOAP faults
    	* src/soup-wsdl-runtime/wsdl-soap-parse.c (wsdl_soap_parse): retrieve
    	SOAP faults from the message being parsed. Added a wsdl_soap_fault
    	parameter, which will be returned if there was actually a SOAP fault
    	in the message
    	* src/soup-wsdl-runtime/wsdl-soap-marshal.c (wsdl_soap_marhal): add
    	a wsdl_soap_fault parameter, to be serialized along with the rest
    	of the SOAP message

--- libsoup/soup.h
+++ libsoup/soup.h
@@ -18,6 +18,7 @@ extern "C" {
 #include <soup/soup-context.h>
 #include <soup/soup-message.h>
 #include <soup/soup-misc.h>
+#include <soup/soup-parser.h>
 #include <soup/soup-serializer.h>
 #include <soup/soup-socket.h>
 #include <soup/soup-uri.h>
