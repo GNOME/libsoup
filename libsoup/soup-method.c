e58e5e0ea70ffcddf6503f844453889339bf5a11
Alex Graveley <orph@src.gnome.org>
2002-03-12 00:47:31 +0000

    Blah. Cvs sucks.

---
+++ libsoup/soup-method.c
@@ -0,0 +1,77 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
+/*
+ * soup-method.c: HTTP Method related processing.
+ *
+ * Authors:
+ *      Alex Graveley (alex@ximian.com)
+ *
+ * Copyright (C) 2001, Ximian, Inc.
+ */
+
+#include <glib.h>
+
+#include "soup-method.h"
+
+SoupMethodId
+soup_method_get_id (const gchar *method)
+{
+	g_return_val_if_fail (method != NULL, SOUP_METHOD_ID_UNKNOWN);
+
+	switch (*method) {
+        case 'H':
+		if (g_strcasecmp (method, "HEAD") == 0)
+			return SOUP_METHOD_ID_HEAD;
+		break;
+        case 'G':
+		if (g_strcasecmp (method, "GET") == 0)
+			return SOUP_METHOD_ID_GET;
+		break;
+        case 'P':
+		if (g_strcasecmp (method, "POST") == 0)
+			return SOUP_METHOD_ID_POST;
+		if (g_strcasecmp (method, "PUT") == 0)
+			return SOUP_METHOD_ID_PUT;
+		if (g_strcasecmp (method, "PATCH") == 0)
+			return SOUP_METHOD_ID_PATCH;
+		if (g_strcasecmp (method, "PROPFIND") == 0)
+			return SOUP_METHOD_ID_PROPFIND;
+		if (g_strcasecmp (method, "PROPPATCH") == 0)
+			return SOUP_METHOD_ID_PROPPATCH;
+		break;
+        case 'D':
+		if (g_strcasecmp (method, "DELETE") == 0)
+			return SOUP_METHOD_ID_DELETE;
+		break;
+        case 'C':
+		if (g_strcasecmp (method, "CONNECT") == 0)
+			return SOUP_METHOD_ID_CONNECT;
+		if (g_strcasecmp (method, "COPY") == 0)
+			return SOUP_METHOD_ID_COPY;
+		break;
+        case 'M':
+		if (g_strcasecmp (method, "MKCOL") == 0)
+			return SOUP_METHOD_ID_MKCOL;
+		if (g_strcasecmp (method, "MOVE") == 0)
+			return SOUP_METHOD_ID_MOVE;
+		break;
+        case 'O':
+		if (g_strcasecmp (method, "OPTIONS") == 0)
+			return SOUP_METHOD_ID_OPTIONS;
+		break;
+        case 'T':
+		if (g_strcasecmp (method, "TRACE") == 0)
+			return SOUP_METHOD_ID_TRACE;
+		break;
+        case 'L':
+		if (g_strcasecmp (method, "LOCK") == 0)
+			return SOUP_METHOD_ID_LOCK;
+		break;
+        case 'U':
+		if (g_strcasecmp (method, "UNLOCK") == 0)
+			return SOUP_METHOD_ID_UNLOCK;
+		break;
+	}
+
+	return SOUP_METHOD_ID_UNKNOWN;
+}
+
---
+++ libsoup/soup-server-auth.h
@@ -0,0 +1,99 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
+/*
+ * soup-server-auth.h: Server-side authentication handling
+ *
+ * Authors:
+ *      Alex Graveley (alex@ximian.com)
+ *
+ * Copyright (C) 2001, Ximian, Inc.
+ */
+
+#ifndef SOUP_SERVER_AUTH_H
+#define SOUP_SERVER_AUTH_H 1
+
+#include <glib.h>
+#include <libsoup/soup-message.h>
+#include <libsoup/soup-misc.h>
+
+typedef union _SoupServerAuth SoupServerAuth;
+typedef struct _SoupServerAuthContext SoupServerAuthContext;
+
+typedef gboolean (*SoupServerAuthCallbackFn) (SoupServerAuthContext *auth_ctx,
+					      SoupServerAuth        *auth,
+					      SoupMessage           *msg, 
+					      gpointer               data);
+
+struct _SoupServerAuthContext {
+	guint                     types;
+	SoupServerAuthCallbackFn  callback;
+	gpointer                  user_data;
+
+	struct {
+		const gchar *realm;
+	} basic_info;
+
+	struct {
+		const gchar *realm;
+		guint        allow_algorithms;
+		gboolean     force_integrity;
+	} digest_info;
+};
+
+void soup_server_auth_context_challenge (SoupServerAuthContext *auth_ctx,
+					 SoupMessage           *msg,
+					 gchar                 *header_name);
+
+
+typedef struct {
+	SoupAuthType  type;
+	const gchar  *user;
+	const gchar  *passwd;
+} SoupServerAuthBasic;
+
+typedef enum {
+	SOUP_ALGORITHM_MD5      = 1 << 0,
+	SOUP_ALGORITHM_MD5_SESS = 1 << 1
+} SoupDigestAlgorithm;
+
+typedef struct {
+	SoupAuthType          type;
+	SoupDigestAlgorithm   algorithm;
+	gboolean              integrity;
+	const gchar          *realm;
+	const gchar          *user;
+	const gchar          *nonce;
+	gint                  nonce_count;
+	const gchar          *cnonce;
+	const gchar          *digest_uri;
+	const gchar          *digest_response;
+	const gchar          *request_method;
+} SoupServerAuthDigest;
+
+typedef struct {
+	SoupAuthType  type;
+	const gchar  *host;
+	const gchar  *domain;
+	const gchar  *user;
+	const gchar  *lm_hash;
+	const gchar  *nt_hash;
+} SoupServerAuthNTLM;
+
+union _SoupServerAuth {
+	SoupAuthType          type;
+	SoupServerAuthBasic   basic;
+	SoupServerAuthDigest  digest;
+	SoupServerAuthNTLM    ntlm;
+};
+
+SoupServerAuth *soup_server_auth_new          (SoupServerAuthContext *auth_ctx, 
+				               const GSList          *auth_hdrs,
+					       SoupMessage           *msg);
+
+void            soup_server_auth_free         (SoupServerAuth        *auth);
+
+const gchar    *soup_server_auth_get_user     (SoupServerAuth        *auth);
+
+gboolean        soup_server_auth_check_passwd (SoupServerAuth        *auth,
+					       gchar                 *passwd);
+
+#endif /* SOUP_SERVER_AUTH_H */
