da4f55e58bfce8673dfa11e6752198af9e7606c8
Dan Winship <danw@src.gnome.org>
2008-04-05 13:26:05 +0000

    Explicitly assign each of the variables to NULL, because that apparently
    	* libsoup/soup-method.c: Explicitly assign each of the variables
    	to NULL, because that apparently causes the OS X linker to treat
    	them differently than if they are left implicitly NULL. #522957.
    
    svn path=/trunk/; revision=1121

--- libsoup/soup-method.c
+++ libsoup/soup-method.c
@@ -9,19 +9,23 @@
 #include <config.h>
 #endif
 
-const char *_SOUP_METHOD_CONNECT;
-const char *_SOUP_METHOD_COPY;
-const char *_SOUP_METHOD_DELETE;
-const char *_SOUP_METHOD_GET;
-const char *_SOUP_METHOD_HEAD;
-const char *_SOUP_METHOD_LOCK;
-const char *_SOUP_METHOD_MKCOL;
-const char *_SOUP_METHOD_MOVE;
-const char *_SOUP_METHOD_OPTIONS;
-const char *_SOUP_METHOD_PATCH;
-const char *_SOUP_METHOD_POST;
-const char *_SOUP_METHOD_PROPFIND;
-const char *_SOUP_METHOD_PROPPATCH;
-const char *_SOUP_METHOD_PUT;
-const char *_SOUP_METHOD_TRACE;
-const char *_SOUP_METHOD_UNLOCK;
+#include "soup-method.h"
+
+/* Explicit assignment to NULL is to help the OS X linker not be
+ * stupid. #522957
+ */
+const char *_SOUP_METHOD_CONNECT = NULL;
+const char *_SOUP_METHOD_COPY = NULL;
+const char *_SOUP_METHOD_DELETE = NULL;
+const char *_SOUP_METHOD_GET = NULL;
+const char *_SOUP_METHOD_HEAD = NULL;
+const char *_SOUP_METHOD_LOCK = NULL;
+const char *_SOUP_METHOD_MKCOL = NULL;
+const char *_SOUP_METHOD_MOVE = NULL;
+const char *_SOUP_METHOD_OPTIONS = NULL;
+const char *_SOUP_METHOD_POST = NULL;
+const char *_SOUP_METHOD_PROPFIND = NULL;
+const char *_SOUP_METHOD_PROPPATCH = NULL;
+const char *_SOUP_METHOD_PUT = NULL;
+const char *_SOUP_METHOD_TRACE = NULL;
+const char *_SOUP_METHOD_UNLOCK = NULL;
