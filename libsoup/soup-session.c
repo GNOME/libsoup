c4d9bb17e8507d87be29067a893ccfcddba3ab69
Dan Winship <danw@src.gnome.org>
2008-09-24 12:19:51 +0000

    merge from trunk 2.24.0.1
    merge from trunk
    	* configure.in: 2.24.0.1
    
    	* NEWS: Update
    
    	* libsoup/soup-session.c (redirect_handler):
    	* libsoup/soup-message.c (soup_message_new):
    	(soup_message_new_from_uri, soup_message_set_uri): Revert the
    	2008-08-25 change; it breaks the rhythmbox DAAP plugin. #553466.
    	To be revisited.
    
    svn path=/branches/gnome-2-24/; revision=1165

--- libsoup/soup-session.c
+++ libsoup/soup-session.c
@@ -819,9 +819,7 @@ redirect_handler (SoupMessage *msg, gpointer user_data)
 	 * are lame, so we use soup_uri_new_with_base().
 	 */
 	new_uri = soup_uri_new_with_base (soup_message_get_uri (msg), new_loc);
-	if (!SOUP_URI_VALID_FOR_HTTP (new_uri)) {
-		if (new_uri)
-			soup_uri_free (new_uri);
+	if (!new_uri) {
 		soup_message_set_status_full (msg,
 					      SOUP_STATUS_MALFORMED,
 					      "Invalid Redirect URL");
