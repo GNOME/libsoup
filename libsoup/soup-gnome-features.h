87d38d7d4a8dc511d92ab88ab4082229b4e3a687
Fridrich Štrba <fridrich.strba@bluewin.ch>
2011-04-13 00:16:38 +0200

    Build relevant parts of libsoup-gnome on windows

--- libsoup/soup-gnome-features.h
+++ libsoup/soup-gnome-features.h
@@ -16,10 +16,12 @@ GType soup_proxy_resolver_gnome_get_type (void);
 GType soup_gnome_features_2_26_get_type (void);
 #define SOUP_TYPE_GNOME_FEATURES_2_26 (soup_gnome_features_2_26_get_type ())
 
+#ifndef G_OS_WIN32
 #ifdef LIBSOUP_I_HAVE_READ_BUG_594377_AND_KNOW_SOUP_PASSWORD_MANAGER_MIGHT_GO_AWAY
 GType soup_password_manager_gnome_get_type (void);
 #define SOUP_TYPE_PASSWORD_MANAGER_GNOME (soup_password_manager_gnome_get_type ())
 #endif /* LIBSOUP_I_HAVE_READ_BUG_594377_AND_KNOW_SOUP_PASSWORD_MANAGER_MIGHT_GO_AWAY */
+#endif
 
 G_END_DECLS
 
