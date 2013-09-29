96da2df64c9dd8cc52e97ce73e54615d6b520664
Dan Winship <danw@gnome.org>
2013-09-29 09:06:37 -0400

    Fix soup_client_input_stream_close to not block
    Closing a SoupClientInputStream for a message that hadn't been
    completely read was trying to read to the end of the message first.
    Fix it to just cancel the read instead.
    
    Also fix a few tests that were implicitly assuming the old behavior.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=695652

--- libsoup/soup-message-io.c
+++ libsoup/soup-message-io.c
@@ -982,7 +982,17 @@ soup_message_io_run_until_finish (SoupMessage   *msg,
 				  GCancellable  *cancellable,
 				  GError       **error)
 {
+	SoupMessagePrivate *priv = SOUP_MESSAGE_GET_PRIVATE (msg);
+	SoupMessageIOData *io = priv->io_data;
+
 	g_object_ref (msg);
+
+	if (io) {
+		g_return_if_fail (io->mode == SOUP_MESSAGE_IO_CLIENT);
+
+		if (io->read_state < SOUP_MESSAGE_IO_STATE_BODY_DONE)
+			io->read_state = SOUP_MESSAGE_IO_STATE_BODY_DONE;
+	}
 
 	if (!io_run_until (msg, blocking,
 			   SOUP_MESSAGE_IO_STATE_DONE,
