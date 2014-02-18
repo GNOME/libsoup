18ae820c81658b44e1ac7daf57df1ee0ef030d53
Slava Monich <slava@monich.com>
2014-02-18 11:38:37 +0200

    soup-session: force port for proxy URL
    Otherwise libsoup is unable to connect through proxies that use default
    port (say, 80 for http). Proxy resolver resolves port-less proxy URLs
    into network address with zero port, which is then passed to connect()

--- libsoup/soup-session.c
+++ libsoup/soup-session.c
@@ -577,7 +577,7 @@ set_proxy_resolver (SoupSession *session, SoupURI *uri,
 		char *uri_string;
 
 		priv->proxy_uri = soup_uri_copy (uri);
-		uri_string = soup_uri_to_string (uri, FALSE);
+		uri_string = soup_uri_to_string_internal (uri, FALSE, TRUE);
 		priv->proxy_resolver = g_simple_proxy_resolver_new (uri_string, NULL);
 		g_free (uri_string);
 	} else if (soup_resolver) {
