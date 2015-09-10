0b0d6ab131a106ee970b0267ec6306b5033728c6
Kalev Lember <klember@redhat.com>
2015-09-10 11:32:36 +0200

    autocleanups: Fix free functions for non-GObject based types
    https://bugzilla.gnome.org/show_bug.cgi?id=754721

--- libsoup/soup-autocleanups.h
+++ libsoup/soup-autocleanups.h
@@ -18,7 +18,10 @@
 #ifndef SOUP_AUTOCLEANUPS_H
 #define SOUP_AUTOCLEANUPS_H
 
+#include <libsoup/soup-cookie.h>
+#include <libsoup/soup-date.h>
 #include <libsoup/soup-types.h>
+#include <libsoup/soup-uri.h>
 
 #if SOUP_VERSION_MAX_ALLOWED >= SOUP_VERSION_2_52
 #ifdef G_DEFINE_AUTOPTR_CLEANUP_FUNC
@@ -26,9 +29,9 @@
 G_DEFINE_AUTOPTR_CLEANUP_FUNC(SoupAddress, g_object_unref)
 G_DEFINE_AUTOPTR_CLEANUP_FUNC(SoupAuth, g_object_unref)
 G_DEFINE_AUTOPTR_CLEANUP_FUNC(SoupAuthDomain, g_object_unref)
-G_DEFINE_AUTOPTR_CLEANUP_FUNC(SoupCookie, g_object_unref)
+G_DEFINE_AUTOPTR_CLEANUP_FUNC(SoupCookie, soup_cookie_free)
 G_DEFINE_AUTOPTR_CLEANUP_FUNC(SoupCookieJar, g_object_unref)
-G_DEFINE_AUTOPTR_CLEANUP_FUNC(SoupDate, g_object_unref)
+G_DEFINE_AUTOPTR_CLEANUP_FUNC(SoupDate, soup_date_free)
 G_DEFINE_AUTOPTR_CLEANUP_FUNC(SoupMessage, g_object_unref)
 G_DEFINE_AUTOPTR_CLEANUP_FUNC(SoupRequest, g_object_unref)
 G_DEFINE_AUTOPTR_CLEANUP_FUNC(SoupRequestHTTP, g_object_unref)
@@ -38,7 +41,7 @@ G_DEFINE_AUTOPTR_CLEANUP_FUNC(SoupSessionAsync, g_object_unref)
 G_DEFINE_AUTOPTR_CLEANUP_FUNC(SoupSessionFeature, g_object_unref)
 G_DEFINE_AUTOPTR_CLEANUP_FUNC(SoupSessionSync, g_object_unref)
 G_DEFINE_AUTOPTR_CLEANUP_FUNC(SoupSocket, g_object_unref)
-G_DEFINE_AUTOPTR_CLEANUP_FUNC(SoupURI, g_object_unref)
+G_DEFINE_AUTOPTR_CLEANUP_FUNC(SoupURI, soup_uri_free)
 G_DEFINE_AUTOPTR_CLEANUP_FUNC(SoupWebsocketConnection, g_object_unref)
 
 #endif
