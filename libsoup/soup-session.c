6fa1eebbf372c668ba8920cd266192890ea1b113
Chun-wei Fan <fanchunwei@src.gnome.org>
2015-09-23 18:40:44 +0800

    soup-session.c: Fix Build on MinGW
    MinGW requires that a prototype be declared for DllMain() before we
    can use it, so define it here to fix the build, as the previous patch to
    add this did not check for this part.  Also fix a build warning on the
    DllMain() as a return BOOL was expected.
    
    This patch was checked with Ignacio Quinteiro (Nacho) via e-mail
    communication.

--- libsoup/soup-session.c
+++ libsoup/soup-session.c
@@ -81,6 +81,10 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 
+BOOL WINAPI DllMain (HINSTANCE hinstDLL,
+                     DWORD     fdwReason,
+                     LPVOID    lpvReserved);
+
 HMODULE soup_dll;
 
 BOOL WINAPI
@@ -99,6 +103,8 @@ DllMain (HINSTANCE hinstDLL,
 		/* do nothing */
 		;
 	}
+
+	return TRUE;
 }
 #endif
 
