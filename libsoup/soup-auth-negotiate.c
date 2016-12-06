GNOME/libsoup

commit bd93c273ea787df4f3fe03c9cf6c8e53d8fc5fc9

Port some of the classes to the new glib private api
We are already depending on the a newly enough glib so
we are now allowed to clean up a bit and use the new private
api.

https://bugzilla.gnome.org/show_bug.cgi?id=774189

libsoup/soup-auth-negotiate.c
@@ -24,22 +24,6 @@
 #include "soup-uri.h"
 
 /**
- * SOUP_TYPE_AUTH_NEGOTIATE:
- *
- * A #GType corresponding to HTTP-based GSS-Negotiate authentication.
- * #SoupSessions do not support this type by default; if you want to
- * enable support for it, call soup_session_add_feature_by_type(),
- * passing %SOUP_TYPE_AUTH_NEGOTIATE.
- *
- * This auth type will only work if libsoup was compiled with GSSAPI
- * support; you can check soup_auth_negotiate_supported() to see if it
- * was.
- *
- * Since: 2.54
- */
-G_DEFINE_TYPE (SoupAuthNegotiate, soup_auth_negotiate, SOUP_TYPE_CONNECTION_AUTH)
-
-/**
  * soup_auth_negotiate_supported:
  *
  * Indicates whether libsoup was built with GSSAPI support. If this is
@@ -90,7 +74,21 @@ typedef struct {
 	SoupNegotiateConnectionState *conn_state;
 } SoupAuthNegotiatePrivate;
 
-#define SOUP_AUTH_NEGOTIATE_GET_PRIVATE(o) (G_TYPE_INSTANCE_GET_PRIVATE ((o), SOUP_TYPE_AUTH_NEGOTIATE, SoupAuthNegotiatePrivate))
+/**
+ * SOUP_TYPE_AUTH_NEGOTIATE:
+ *
+ * A #GType corresponding to HTTP-based GSS-Negotiate authentication.
+ * #SoupSessions do not support this type by default; if you want to
+ * enable support for it, call soup_session_add_feature_by_type(),
+ * passing %SOUP_TYPE_AUTH_NEGOTIATE.
+ *
+ * This auth type will only work if libsoup was compiled with GSSAPI
+ * support; you can check soup_auth_negotiate_supported() to see if it
+ * was.
+ *
+ * Since: 2.54
+ */
+G_DEFINE_TYPE_WITH_PRIVATE (SoupAuthNegotiate, soup_auth_negotiate, SOUP_TYPE_CONNECTION_AUTH)
 
 static gboolean check_auth_trusted_uri (SoupConnectionAuth *auth,
 					SoupMessage *msg);
@@ -116,7 +114,7 @@ static const gss_OID_desc gss_mech_spnego = { sizeof (spnego_OID) - 1, (void *) 
 static gpointer
 soup_auth_negotiate_create_connection_state (SoupConnectionAuth *auth)
 {
-	SoupAuthNegotiatePrivate *priv = SOUP_AUTH_NEGOTIATE_GET_PRIVATE (auth);
+	SoupAuthNegotiatePrivate *priv = soup_auth_negotiate_get_instance_private (SOUP_AUTH_NEGOTIATE (auth));
 	SoupNegotiateConnectionState *conn;
 
 	conn = g_slice_new0 (SoupNegotiateConnectionState);
@@ -138,7 +136,7 @@ soup_auth_negotiate_free_connection_state (SoupConnectionAuth *auth,
 					   gpointer state)
 {
 	SoupAuthNegotiate *negotiate = SOUP_AUTH_NEGOTIATE (auth);
-	SoupAuthNegotiatePrivate *priv = SOUP_AUTH_NEGOTIATE_GET_PRIVATE (negotiate);
+	SoupAuthNegotiatePrivate *priv = soup_auth_negotiate_get_instance_private (negotiate);
 	SoupNegotiateConnectionState *conn = state;
 
 	free_connection_state_data (conn);
@@ -169,7 +167,7 @@ soup_auth_negotiate_authenticate (SoupAuth *auth, const char *username,
 				  const char *password)
 {
 	SoupAuthNegotiate *negotiate = SOUP_AUTH_NEGOTIATE (auth);
-	SoupAuthNegotiatePrivate *priv = SOUP_AUTH_NEGOTIATE_GET_PRIVATE (negotiate);
+	SoupAuthNegotiatePrivate *priv = soup_auth_negotiate_get_instance_private (negotiate);
 
 	/* It is not possible to authenticate with username and password. */
 	priv->is_authenticated = FALSE;
@@ -179,7 +177,7 @@ static gboolean
 soup_auth_negotiate_is_authenticated (SoupAuth *auth)
 {
 	SoupAuthNegotiate *negotiate = SOUP_AUTH_NEGOTIATE (auth);
-	SoupAuthNegotiatePrivate *priv = SOUP_AUTH_NEGOTIATE_GET_PRIVATE (negotiate);
+	SoupAuthNegotiatePrivate *priv = soup_auth_negotiate_get_instance_private (negotiate);
 
 	/* We are authenticated just in case we received the GSS_S_COMPLETE. */
 	return priv->is_authenticated;
@@ -226,7 +224,7 @@ soup_auth_negotiate_update_connection (SoupConnectionAuth *auth, SoupMessage *ms
 #ifdef LIBSOUP_HAVE_GSSAPI
 	gboolean success = TRUE;
 	SoupNegotiateConnectionState *conn = state;
-	SoupAuthNegotiatePrivate *priv = SOUP_AUTH_NEGOTIATE_GET_PRIVATE (auth);
+	SoupAuthNegotiatePrivate *priv = soup_auth_negotiate_get_instance_private (SOUP_AUTH_NEGOTIATE (auth));
 	GError *err = NULL;
 
 	if (!check_auth_trusted_uri (auth, msg)) {
@@ -317,8 +315,6 @@ soup_auth_negotiate_class_init (SoupAuthNegotiateClass *auth_negotiate_class)
 	auth_class->is_authenticated = soup_auth_negotiate_is_authenticated;
 	auth_class->can_authenticate = soup_auth_negotiate_can_authenticate;
 
-	g_type_class_add_private (auth_negotiate_class, sizeof (SoupAuthNegotiatePrivate));
-
 	parse_uris_from_env_variable ("SOUP_GSSAPI_TRUSTED_URIS", &trusted_uris);
 	parse_uris_from_env_variable ("SOUP_GSSAPI_BLACKLISTED_URIS", &blacklisted_uris);
 #endif /* LIBSOUP_HAVE_GSSAPI */
@@ -332,7 +328,7 @@ check_server_response (SoupMessage *msg, gpointer auth)
 	const char *auth_headers;
 	GError *err = NULL;
 	SoupAuthNegotiate *negotiate = auth;
-	SoupAuthNegotiatePrivate *priv = SOUP_AUTH_NEGOTIATE_GET_PRIVATE (negotiate);
+	SoupAuthNegotiatePrivate *priv = soup_auth_negotiate_get_instance_private (negotiate);
 	SoupNegotiateConnectionState *conn = priv->conn_state;
 
 	if (auth != soup_message_get_auth (msg))
@@ -369,7 +365,7 @@ static void
 remove_server_response_handler (SoupMessage *msg, gpointer auth)
 {
 	SoupAuthNegotiate *negotiate = auth;
-	SoupAuthNegotiatePrivate *priv = SOUP_AUTH_NEGOTIATE_GET_PRIVATE (negotiate);
+	SoupAuthNegotiatePrivate *priv = soup_auth_negotiate_get_instance_private (negotiate);
 
 	g_signal_handler_disconnect (msg, priv->message_got_headers_signal_id);
 	priv->message_got_headers_signal_id = 0;

libsoup/soup-address.c
@@ -53,7 +53,6 @@ typedef struct {
 
 	GMutex lock;
 } SoupAddressPrivate;
-#define SOUP_ADDRESS_GET_PRIVATE(o) (G_TYPE_INSTANCE_GET_PRIVATE ((o), SOUP_TYPE_ADDRESS, SoupAddressPrivate))
 
 /* sockaddr generic macros */
 #define SOUP_SIN(priv) ((struct sockaddr_in *)priv->sockaddr)
@@ -98,13 +97,14 @@ typedef struct {
 static void soup_address_connectable_iface_init (GSocketConnectableIface *connectable_iface);
 
 G_DEFINE_TYPE_WITH_CODE (SoupAddress, soup_address, G_TYPE_OBJECT,
+                         G_ADD_PRIVATE (SoupAddress)
 			 G_IMPLEMENT_INTERFACE (G_TYPE_SOCKET_CONNECTABLE,
 						soup_address_connectable_iface_init))
 
 static void
 soup_address_init (SoupAddress *addr)
 {
-	SoupAddressPrivate *priv = SOUP_ADDRESS_GET_PRIVATE (addr);
+	SoupAddressPrivate *priv = soup_address_get_instance_private (addr);
 
 	g_mutex_init (&priv->lock);
 }
@@ -113,7 +113,7 @@ static void
 soup_address_finalize (GObject *object)
 {
 	SoupAddress *addr = SOUP_ADDRESS (object);
-	SoupAddressPrivate *priv = SOUP_ADDRESS_GET_PRIVATE (addr);
+	SoupAddressPrivate *priv = soup_address_get_instance_private (addr);
 
 	g_free (priv->sockaddr);
 	g_free (priv->name);
@@ -136,7 +136,7 @@ soup_address_constructor (GType                  type,
 		type, n_construct_properties, construct_properties);
 	if (!addr)
 		return NULL;
-	priv = SOUP_ADDRESS_GET_PRIVATE (addr);
+	priv = soup_address_get_instance_private (SOUP_ADDRESS (addr));
 
 	if (!priv->name && !priv->sockaddr) {
 		g_object_unref (addr);
@@ -150,7 +150,7 @@ static void
 soup_address_set_property (GObject *object, guint prop_id,
 			   const GValue *value, GParamSpec *pspec)
 {
-	SoupAddressPrivate *priv = SOUP_ADDRESS_GET_PRIVATE (object);
+	SoupAddressPrivate *priv = soup_address_get_instance_private (SOUP_ADDRESS (object));
 	SoupAddressFamily family;
 	struct sockaddr *sa;
 	int len, port;
@@ -214,7 +214,7 @@ static void
 soup_address_get_property (GObject *object, guint prop_id,
 			   GValue *value, GParamSpec *pspec)
 {
-	SoupAddressPrivate *priv = SOUP_ADDRESS_GET_PRIVATE (object);
+	SoupAddressPrivate *priv = soup_address_get_instance_private (SOUP_ADDRESS (object));
 
 	switch (prop_id) {
 	case PROP_NAME:
@@ -248,8 +248,6 @@ static void
 soup_address_class_init (SoupAddressClass *address_class)
 {
 	GObjectClass *object_class = G_OBJECT_CLASS (address_class);
-
-	g_type_class_add_private (address_class, sizeof (SoupAddressPrivate));
 
 	/* virtual method override */
 	object_class->constructor  = soup_address_constructor;
@@ -450,9 +448,12 @@ soup_address_new_any (SoupAddressFamily family, guint port)
 const char *
 soup_address_get_name (SoupAddress *addr)
 {
-	g_return_val_if_fail (SOUP_IS_ADDRESS (addr), NULL);
+	SoupAddressPrivate *priv;
 
-	return SOUP_ADDRESS_GET_PRIVATE (addr)->name;
+	g_return_val_if_fail (SOUP_IS_ADDRESS (addr), NULL);
+	priv = soup_address_get_instance_private (addr);
+
+	return priv->name;
 }
 
 /**
@@ -476,7 +477,7 @@ soup_address_get_sockaddr (SoupAddress *addr, int *len)
 	SoupAddressPrivate *priv;
 
 	g_return_val_if_fail (SOUP_IS_ADDRESS (addr), NULL);
-	priv = SOUP_ADDRESS_GET_PRIVATE (addr);
+	priv = soup_address_get_instance_private (addr);
 
 	if (priv->sockaddr && len)
 		*len = SOUP_ADDRESS_FAMILY_SOCKADDR_SIZE (SOUP_ADDRESS_GET_FAMILY (priv));
@@ -497,7 +498,7 @@ soup_address_get_sockaddr (SoupAddress *addr, int *len)
 GSocketAddress *
 soup_address_get_gsockaddr (SoupAddress *addr)
 {
-	SoupAddressPrivate *priv = SOUP_ADDRESS_GET_PRIVATE (addr);
+	SoupAddressPrivate *priv = soup_address_get_instance_private (addr);
 
 	return g_socket_address_new_from_native (priv->sockaddr,
 						 SOUP_ADDRESS_FAMILY_SOCKADDR_SIZE (SOUP_ADDRESS_GET_FAMILY (priv)));
@@ -506,7 +507,7 @@ soup_address_get_gsockaddr (SoupAddress *addr)
 static GInetAddress *
 soup_address_make_inet_address (SoupAddress *addr)
 {
-	SoupAddressPrivate *priv = SOUP_ADDRESS_GET_PRIVATE (addr);
+	SoupAddressPrivate *priv = soup_address_get_instance_private (addr);
 	GSocketAddress *gsa;
 	GInetAddress *gia;
 
@@ -538,7 +539,7 @@ soup_address_get_physical (SoupAddress *addr)
 	SoupAddressPrivate *priv;
 
 	g_return_val_if_fail (SOUP_IS_ADDRESS (addr), NULL);
-	priv = SOUP_ADDRESS_GET_PRIVATE (addr);
+	priv = soup_address_get_instance_private (addr);
 
 	if (!priv->sockaddr)
 		return NULL;
@@ -565,9 +566,12 @@ soup_address_get_physical (SoupAddress *addr)
 guint
 soup_address_get_port (SoupAddress *addr)
 {
-	g_return_val_if_fail (SOUP_IS_ADDRESS (addr), 0);
+	SoupAddressPrivate *priv;
 
-	return SOUP_ADDRESS_GET_PRIVATE (addr)->port;
+	g_return_val_if_fail (SOUP_IS_ADDRESS (addr), 0);
+	priv = soup_address_get_instance_private (addr);
+
+	return priv->port;
 }
 
 
@@ -577,7 +581,7 @@ soup_address_get_port (SoupAddress *addr)
 static void
 maybe_resolve_ip (SoupAddress *addr)
 {
-	SoupAddressPrivate *priv = SOUP_ADDRESS_GET_PRIVATE (addr);
+	SoupAddressPrivate *priv = soup_address_get_instance_private (addr);
 	const char *pct, *ip;
 	char *tmp = NULL;
 	GSocketConnectable *gaddr;
@@ -627,7 +631,7 @@ maybe_resolve_ip (SoupAddress *addr)
 static guint
 update_addrs (SoupAddress *addr, GList *addrs, GError *error)
 {
-	SoupAddressPrivate *priv = SOUP_ADDRESS_GET_PRIVATE (addr);
+	SoupAddressPrivate *priv = soup_address_get_instance_private (addr);
 	GInetAddress *gia;
 	GSocketAddress *gsa;
 	int i;
@@ -666,7 +670,7 @@ update_addrs (SoupAddress *addr, GList *addrs, GError *error)
 static guint
 update_name (SoupAddress *addr, const char *name, GError *error)
 {
-	SoupAddressPrivate *priv = SOUP_ADDRESS_GET_PRIVATE (addr);
+	SoupAddressPrivate *priv = soup_address_get_instance_private (addr);
 
 	if (error) {
 		if (error->domain == G_IO_ERROR &&
@@ -725,7 +729,7 @@ lookup_resolved (GObject *source, GAsyncResult *result, gpointer user_data)
 	GResolver *resolver = G_RESOLVER (source);
 	SoupAddressResolveAsyncData *res_data = user_data;
 	SoupAddress *addr = res_data->addr;
-	SoupAddressPrivate *priv = SOUP_ADDRESS_GET_PRIVATE (addr);
+	SoupAddressPrivate *priv = soup_address_get_instance_private (addr);
 	GError *error = NULL;
 	guint status;
 
@@ -809,7 +813,7 @@ soup_address_resolve_async (SoupAddress *addr, GMainContext *async_context,
 	GResolver *resolver;
 
 	g_return_if_fail (SOUP_IS_ADDRESS (addr));
-	priv = SOUP_ADDRESS_GET_PRIVATE (addr);
+	priv = soup_address_get_instance_private (addr);
 	g_return_if_fail (priv->name || priv->sockaddr);
 
 	/* We don't need to do locking here because the async case is
@@ -858,7 +862,7 @@ soup_address_resolve_async (SoupAddress *addr, GMainContext *async_context,
 static guint
 resolve_sync_internal (SoupAddress *addr, GCancellable *cancellable, GError **error)
 {
-	SoupAddressPrivate *priv = SOUP_ADDRESS_GET_PRIVATE (addr);
+	SoupAddressPrivate *priv = soup_address_get_instance_private (addr);
 	GResolver *resolver;
 	guint status;
 	GError *my_err = NULL;
@@ -937,7 +941,7 @@ soup_address_resolve_sync (SoupAddress *addr, GCancellable *cancellable)
 	SoupAddressPrivate *priv;
 
 	g_return_val_if_fail (SOUP_IS_ADDRESS (addr), SOUP_STATUS_MALFORMED);
-	priv = SOUP_ADDRESS_GET_PRIVATE (addr);
+	priv = soup_address_get_instance_private (addr);
 	g_return_val_if_fail (priv->name || priv->sockaddr, SOUP_STATUS_MALFORMED);
 
 	return resolve_sync_internal (addr, cancellable, NULL);
@@ -960,7 +964,7 @@ soup_address_is_resolved (SoupAddress *addr)
 	gboolean resolved;
 
 	g_return_val_if_fail (SOUP_IS_ADDRESS (addr), FALSE);
-	priv = SOUP_ADDRESS_GET_PRIVATE (addr);
+	priv = soup_address_get_instance_private (addr);
 
 	g_mutex_lock (&priv->lock);
 	resolved = priv->sockaddr && priv->name;
@@ -983,7 +987,7 @@ soup_address_is_resolved (SoupAddress *addr)
 guint
 soup_address_hash_by_name (gconstpointer addr)
 {
-	SoupAddressPrivate *priv = SOUP_ADDRESS_GET_PRIVATE (addr);
+	SoupAddressPrivate *priv = soup_address_get_instance_private (SOUP_ADDRESS (addr));
 
 	g_return_val_if_fail (priv->name != NULL, 0);
 	return g_str_hash (priv->name);
@@ -1024,8 +1028,8 @@ soup_address_hash_by_name (gconstpointer addr)
 gboolean
 soup_address_equal_by_name (gconstpointer addr1, gconstpointer addr2)
 {
-	SoupAddressPrivate *priv1 = SOUP_ADDRESS_GET_PRIVATE (addr1);
-	SoupAddressPrivate *priv2 = SOUP_ADDRESS_GET_PRIVATE (addr2);
+	SoupAddressPrivate *priv1 = soup_address_get_instance_private (SOUP_ADDRESS (addr1));
+	SoupAddressPrivate *priv2 = soup_address_get_instance_private (SOUP_ADDRESS (addr2));
 
 	g_return_val_if_fail (priv1->name != NULL, FALSE);
 	g_return_val_if_fail (priv2->name != NULL, FALSE);
@@ -1046,7 +1050,7 @@ soup_address_equal_by_name (gconstpointer addr1, gconstpointer addr2)
 guint
 soup_address_hash_by_ip (gconstpointer addr)
 {
-	SoupAddressPrivate *priv = SOUP_ADDRESS_GET_PRIVATE (addr);
+	SoupAddressPrivate *priv = soup_address_get_instance_private (SOUP_ADDRESS (addr));
 	guint hash;
 
 	g_return_val_if_fail (priv->sockaddr != NULL, 0);
@@ -1084,8 +1088,8 @@ soup_address_hash_by_ip (gconstpointer addr)
 gboolean
 soup_address_equal_by_ip (gconstpointer addr1, gconstpointer addr2)
 {
-	SoupAddressPrivate *priv1 = SOUP_ADDRESS_GET_PRIVATE (addr1);
-	SoupAddressPrivate *priv2 = SOUP_ADDRESS_GET_PRIVATE (addr2);
+	SoupAddressPrivate *priv1 = soup_address_get_instance_private (SOUP_ADDRESS (addr1));
+	SoupAddressPrivate *priv2 = soup_address_get_instance_private (SOUP_ADDRESS (addr2));
 	int size;
 
 	g_return_val_if_fail (priv1->sockaddr != NULL, FALSE);
@@ -1131,7 +1135,7 @@ soup_address_address_enumerator_finalize (GObject *object)
 static GSocketAddress *
 next_address (SoupAddressAddressEnumerator *addr_enum)
 {
-	SoupAddressPrivate *priv = SOUP_ADDRESS_GET_PRIVATE (addr_enum->addr);
+	SoupAddressPrivate *priv = soup_address_get_instance_private (addr_enum->addr);
 	struct sockaddr_storage *ss;
 	int next_addr;
 
@@ -1159,7 +1163,7 @@ soup_address_address_enumerator_next (GSocketAddressEnumerator  *enumerator,
 {
 	SoupAddressAddressEnumerator *addr_enum =
 		SOUP_ADDRESS_ADDRESS_ENUMERATOR (enumerator);
-	SoupAddressPrivate *priv = SOUP_ADDRESS_GET_PRIVATE (addr_enum->addr);
+	SoupAddressPrivate *priv = soup_address_get_instance_private (addr_enum->addr);
 
 	if (!priv->sockaddr) {
 		if (resolve_sync_internal (addr_enum->addr, cancellable, error) != SOUP_STATUS_OK)
@@ -1195,7 +1199,7 @@ soup_address_address_enumerator_next_async (GSocketAddressEnumerator  *enumerato
 {
 	SoupAddressAddressEnumerator *addr_enum =
 		SOUP_ADDRESS_ADDRESS_ENUMERATOR (enumerator);
-	SoupAddressPrivate *priv = SOUP_ADDRESS_GET_PRIVATE (addr_enum->addr);
+	SoupAddressPrivate *priv = soup_address_get_instance_private (addr_enum->addr);
 	GTask *task;
 
 	task = g_task_new (enumerator, cancellable, callback, user_data);
@@ -1244,7 +1248,7 @@ soup_address_connectable_enumerate (GSocketConnectable *connectable)
 	addr_enum = g_object_new (SOUP_TYPE_ADDRESS_ADDRESS_ENUMERATOR, NULL);
 	addr_enum->addr = g_object_ref (connectable);
 
-	priv = SOUP_ADDRESS_GET_PRIVATE (addr_enum->addr);
+	priv = soup_address_get_instance_private (addr_enum->addr);
 	addr_enum->orig_offset = priv->offset;
 
 	return (GSocketAddressEnumerator *)addr_enum;
@@ -1254,7 +1258,7 @@ static GSocketAddressEnumerator *
 soup_address_connectable_proxy_enumerate (GSocketConnectable *connectable)
 {
 	SoupAddress *addr = SOUP_ADDRESS (connectable);
-	SoupAddressPrivate *priv = SOUP_ADDRESS_GET_PRIVATE (addr);
+	SoupAddressPrivate *priv = soup_address_get_instance_private (addr);
 	GSocketAddressEnumerator *proxy_enum;
 	SoupURI *uri;
 	char *uri_string;

libsoup/soup-auth-basic.c
@@ -17,7 +17,6 @@
 typedef struct {
 	char *token;
 } SoupAuthBasicPrivate;
-#define SOUP_AUTH_BASIC_GET_PRIVATE(o) (G_TYPE_INSTANCE_GET_PRIVATE ((o), SOUP_TYPE_AUTH_BASIC, SoupAuthBasicPrivate))
 
 /**
  * SOUP_TYPE_AUTH_BASIC:
@@ -30,7 +29,7 @@ typedef struct {
  * Since: 2.34
  */
 
-G_DEFINE_TYPE (SoupAuthBasic, soup_auth_basic, SOUP_TYPE_AUTH)
+G_DEFINE_TYPE_WITH_PRIVATE (SoupAuthBasic, soup_auth_basic, SOUP_TYPE_AUTH)
 
 static void
 soup_auth_basic_init (SoupAuthBasic *basic)
@@ -40,7 +39,7 @@ soup_auth_basic_init (SoupAuthBasic *basic)
 static void
 soup_auth_basic_finalize (GObject *object)
 {
-	SoupAuthBasicPrivate *priv = SOUP_AUTH_BASIC_GET_PRIVATE (object);
+	SoupAuthBasicPrivate *priv = soup_auth_basic_get_instance_private (SOUP_AUTH_BASIC (object));
 
 	g_free (priv->token);
 
@@ -51,7 +50,7 @@ static gboolean
 soup_auth_basic_update (SoupAuth *auth, SoupMessage *msg,
 			GHashTable *auth_params)
 {
-	SoupAuthBasicPrivate *priv = SOUP_AUTH_BASIC_GET_PRIVATE (auth);
+	SoupAuthBasicPrivate *priv = soup_auth_basic_get_instance_private (SOUP_AUTH_BASIC (auth));
 
 	/* If we're updating a pre-existing auth, the
 	 * username/password must be bad now, so forget it.
@@ -87,7 +86,7 @@ static void
 soup_auth_basic_authenticate (SoupAuth *auth, const char *username,
 			      const char *password)
 {
-	SoupAuthBasicPrivate *priv = SOUP_AUTH_BASIC_GET_PRIVATE (auth);
+	SoupAuthBasicPrivate *priv = soup_auth_basic_get_instance_private (SOUP_AUTH_BASIC (auth));
 	char *user_pass, *user_pass_latin1;
 	int len;
 
@@ -114,13 +113,15 @@ soup_auth_basic_authenticate (SoupAuth *auth, const char *username,
 static gboolean
 soup_auth_basic_is_authenticated (SoupAuth *auth)
 {
-	return SOUP_AUTH_BASIC_GET_PRIVATE (auth)->token != NULL;
+	SoupAuthBasicPrivate *priv = soup_auth_basic_get_instance_private (SOUP_AUTH_BASIC (auth));
+
+	return priv->token != NULL;
 }
 
 static char *
 soup_auth_basic_get_authorization (SoupAuth *auth, SoupMessage *msg)
 {
-	SoupAuthBasicPrivate *priv = SOUP_AUTH_BASIC_GET_PRIVATE (auth);
+	SoupAuthBasicPrivate *priv = soup_auth_basic_get_instance_private (SOUP_AUTH_BASIC (auth));
 
 	return g_strdup_printf ("Basic %s", priv->token);
 }
@@ -130,8 +131,6 @@ soup_auth_basic_class_init (SoupAuthBasicClass *auth_basic_class)
 {
 	SoupAuthClass *auth_class = SOUP_AUTH_CLASS (auth_basic_class);
 	GObjectClass *object_class = G_OBJECT_CLASS (auth_basic_class);
-
-	g_type_class_add_private (auth_basic_class, sizeof (SoupAuthBasicPrivate));
 
 	auth_class->scheme_name = "Basic";
 	auth_class->strength = 1;

libsoup/soup-auth-digest.c
@@ -36,7 +36,6 @@ typedef struct {
 	int                      nc;
 	SoupAuthDigestQop        qop;
 } SoupAuthDigestPrivate;
-#define SOUP_AUTH_DIGEST_GET_PRIVATE(o) (G_TYPE_INSTANCE_GET_PRIVATE ((o), SOUP_TYPE_AUTH_DIGEST, SoupAuthDigestPrivate))
 
 static void recompute_hex_a1 (SoupAuthDigestPrivate *priv);
 
@@ -51,7 +50,7 @@ static void recompute_hex_a1 (SoupAuthDigestPrivate *priv);
  * Since: 2.34
  */
 
-G_DEFINE_TYPE (SoupAuthDigest, soup_auth_digest, SOUP_TYPE_AUTH)
+G_DEFINE_TYPE_WITH_PRIVATE (SoupAuthDigest, soup_auth_digest, SOUP_TYPE_AUTH)
 
 static void
 soup_auth_digest_init (SoupAuthDigest *digest)
@@ -61,7 +60,7 @@ soup_auth_digest_init (SoupAuthDigest *digest)
 static void
 soup_auth_digest_finalize (GObject *object)
 {
-	SoupAuthDigestPrivate *priv = SOUP_AUTH_DIGEST_GET_PRIVATE (object);
+	SoupAuthDigestPrivate *priv = soup_auth_digest_get_instance_private (SOUP_AUTH_DIGEST (object));
 
 	g_free (priv->user);
 	g_free (priv->nonce);
@@ -137,7 +136,8 @@ static gboolean
 soup_auth_digest_update (SoupAuth *auth, SoupMessage *msg,
 			 GHashTable *auth_params)
 {
-	SoupAuthDigestPrivate *priv = SOUP_AUTH_DIGEST_GET_PRIVATE (auth);
+	SoupAuthDigest *auth_digest = SOUP_AUTH_DIGEST (auth);
+	SoupAuthDigestPrivate *priv = soup_auth_digest_get_instance_private (auth_digest);
 	const char *stale, *qop;
 	guint qop_options;
 	gboolean ok = TRUE;
@@ -184,7 +184,8 @@ soup_auth_digest_update (SoupAuth *auth, SoupMessage *msg,
 static GSList *
 soup_auth_digest_get_protection_space (SoupAuth *auth, SoupURI *source_uri)
 {
-	SoupAuthDigestPrivate *priv = SOUP_AUTH_DIGEST_GET_PRIVATE (auth);
+	SoupAuthDigest *auth_digest = SOUP_AUTH_DIGEST (auth);
+	SoupAuthDigestPrivate *priv = soup_auth_digest_get_instance_private (auth_digest);
 	GSList *space = NULL;
 	SoupURI *uri;
 	char **dvec, *d, *dir, *slash;
@@ -290,7 +291,8 @@ static void
 soup_auth_digest_authenticate (SoupAuth *auth, const char *username,
 			       const char *password)
 {
-	SoupAuthDigestPrivate *priv = SOUP_AUTH_DIGEST_GET_PRIVATE (auth);
+	SoupAuthDigest *auth_digest = SOUP_AUTH_DIGEST (auth);
+	SoupAuthDigestPrivate *priv = soup_auth_digest_get_instance_private (auth_digest);
 	char *bgen;
 
 	g_clear_pointer (&priv->cnonce, g_free);
@@ -318,7 +320,9 @@ soup_auth_digest_authenticate (SoupAuth *auth, const char *username,
 static gboolean
 soup_auth_digest_is_authenticated (SoupAuth *auth)
 {
-	return SOUP_AUTH_DIGEST_GET_PRIVATE (auth)->cnonce != NULL;
+	SoupAuthDigestPrivate *priv = soup_auth_digest_get_instance_private (SOUP_AUTH_DIGEST (auth));
+
+	return priv->cnonce != NULL;
 }
 
 void
@@ -373,7 +377,8 @@ static void
 authentication_info_cb (SoupMessage *msg, gpointer data)
 {
 	SoupAuth *auth = data;
-	SoupAuthDigestPrivate *priv = SOUP_AUTH_DIGEST_GET_PRIVATE (auth);
+	SoupAuthDigest *auth_digest = SOUP_AUTH_DIGEST (auth);
+	SoupAuthDigestPrivate *priv = soup_auth_digest_get_instance_private (auth_digest);
 	const char *header;
 	GHashTable *auth_params;
 	char *nextnonce;
@@ -403,7 +408,8 @@ authentication_info_cb (SoupMessage *msg, gpointer data)
 static char *
 soup_auth_digest_get_authorization (SoupAuth *auth, SoupMessage *msg)
 {
-	SoupAuthDigestPrivate *priv = SOUP_AUTH_DIGEST_GET_PRIVATE (auth);
+	SoupAuthDigest *auth_digest = SOUP_AUTH_DIGEST (auth);
+	SoupAuthDigestPrivate *priv = soup_auth_digest_get_instance_private (auth_digest);
 	char response[33], *token;
 	char *url, *algorithm;
 	GString *out;
@@ -470,8 +476,6 @@ soup_auth_digest_class_init (SoupAuthDigestClass *auth_digest_class)
 {
 	SoupAuthClass *auth_class = SOUP_AUTH_CLASS (auth_digest_class);
 	GObjectClass *object_class = G_OBJECT_CLASS (auth_digest_class);
-
-	g_type_class_add_private (auth_digest_class, sizeof (SoupAuthDigestPrivate));
 
 	auth_class->scheme_name = "Digest";
 	auth_class->strength = 5;

libsoup/soup-auth-domain-basic.c
@@ -37,9 +37,7 @@ typedef struct {
 	GDestroyNotify auth_dnotify;
 } SoupAuthDomainBasicPrivate;
 
-#define SOUP_AUTH_DOMAIN_BASIC_GET_PRIVATE(o) (G_TYPE_INSTANCE_GET_PRIVATE ((o), SOUP_TYPE_AUTH_DOMAIN_BASIC, SoupAuthDomainBasicPrivate))
-
-G_DEFINE_TYPE (SoupAuthDomainBasic, soup_auth_domain_basic, SOUP_TYPE_AUTH_DOMAIN)
+G_DEFINE_TYPE_WITH_PRIVATE (SoupAuthDomainBasic, soup_auth_domain_basic, SOUP_TYPE_AUTH_DOMAIN)
 
 static void
 soup_auth_domain_basic_init (SoupAuthDomainBasic *basic)
@@ -50,7 +48,7 @@ static void
 soup_auth_domain_basic_finalize (GObject *object)
 {
 	SoupAuthDomainBasicPrivate *priv =
-		SOUP_AUTH_DOMAIN_BASIC_GET_PRIVATE (object);
+		soup_auth_domain_basic_get_instance_private (SOUP_AUTH_DOMAIN_BASIC (object));
 
 	if (priv->auth_dnotify)
 		priv->auth_dnotify (priv->auth_data);
@@ -63,7 +61,7 @@ soup_auth_domain_basic_set_property (GObject *object, guint prop_id,
 				     const GValue *value, GParamSpec *pspec)
 {
 	SoupAuthDomainBasicPrivate *priv =
-		SOUP_AUTH_DOMAIN_BASIC_GET_PRIVATE (object);
+		soup_auth_domain_basic_get_instance_private (SOUP_AUTH_DOMAIN_BASIC (object));
 
 	switch (prop_id) {
 	case PROP_AUTH_CALLBACK:
@@ -87,7 +85,7 @@ soup_auth_domain_basic_get_property (GObject *object, guint prop_id,
 				     GValue *value, GParamSpec *pspec)
 {
 	SoupAuthDomainBasicPrivate *priv =
-		SOUP_AUTH_DOMAIN_BASIC_GET_PRIVATE (object);
+		soup_auth_domain_basic_get_instance_private (SOUP_AUTH_DOMAIN_BASIC (object));
 
 	switch (prop_id) {
 	case PROP_AUTH_CALLBACK:
@@ -181,7 +179,7 @@ soup_auth_domain_basic_set_auth_callback (SoupAuthDomain *domain,
 					  GDestroyNotify  dnotify)
 {
 	SoupAuthDomainBasicPrivate *priv =
-		SOUP_AUTH_DOMAIN_BASIC_GET_PRIVATE (domain);
+		soup_auth_domain_basic_get_instance_private (SOUP_AUTH_DOMAIN_BASIC (domain));
 
 	if (priv->auth_dnotify)
 		priv->auth_dnotify (priv->auth_data);
@@ -234,7 +232,7 @@ soup_auth_domain_basic_accepts (SoupAuthDomain *domain, SoupMessage *msg,
 				const char *header)
 {
 	SoupAuthDomainBasicPrivate *priv =
-		SOUP_AUTH_DOMAIN_BASIC_GET_PRIVATE (domain);
+		soup_auth_domain_basic_get_instance_private (SOUP_AUTH_DOMAIN_BASIC (domain));
 	char *username, *password;
 	gboolean ok = FALSE;
 
@@ -298,8 +296,6 @@ soup_auth_domain_basic_class_init (SoupAuthDomainBasicClass *basic_class)
 	SoupAuthDomainClass *auth_domain_class =
 		SOUP_AUTH_DOMAIN_CLASS (basic_class);
 	GObjectClass *object_class = G_OBJECT_CLASS (basic_class);
-
-	g_type_class_add_private (basic_class, sizeof (SoupAuthDomainBasicPrivate));
 
 	auth_domain_class->accepts        = soup_auth_domain_basic_accepts;
 	auth_domain_class->challenge      = soup_auth_domain_basic_challenge;

commit 975e6f5426568187e907926c905bcb554019ae9f

Define SoupAuthNegotiate unconditionally
This class doesn't do much if LIBSOUP_HAVE_GSSAPI is not defined, but
the type itself still needs to exist, or (among other things) the
introspection build will fail.

bd93c273ea787df4f3fe03c9cf6c8e53d8fc5fc9 broke that, by making the
G_DEFINE_TYPE() condition on #ifdef LIBSOUP_HAVE_GSSAPI.  This was done
because the Private struct contains gss types.

Reorder things a bit to make sure we always do the G_DEFINE_TYPE() even
if we have no GSSAPI.

https://bugzilla.gnome.org/show_bug.cgi?id=774189

libsoup/soup-auth-negotiate.c
@@ -43,7 +43,6 @@ soup_auth_negotiate_supported (void)
 #endif
 }
 
-#ifdef LIBSOUP_HAVE_GSSAPI
 #define AUTH_GSS_ERROR      -1
 #define AUTH_GSS_COMPLETE    1
 #define AUTH_GSS_CONTINUE    0
@@ -59,8 +58,10 @@ typedef struct {
 	gboolean initialized;
 	gchar *response_header;
 
+#ifdef LIBSOUP_HAVE_GSSAPI
 	gss_ctx_id_t context;
 	gss_name_t   server_name;
+#endif
 
 	SoupNegotiateState state;
 } SoupNegotiateConnectionState;
@@ -90,6 +91,7 @@ typedef struct {
  */
 G_DEFINE_TYPE_WITH_PRIVATE (SoupAuthNegotiate, soup_auth_negotiate, SOUP_TYPE_CONNECTION_AUTH)
 
+#ifdef LIBSOUP_HAVE_GSSAPI
 static gboolean check_auth_trusted_uri (SoupConnectionAuth *auth,
 					SoupMessage *msg);
 static gboolean soup_gss_build_response (SoupNegotiateConnectionState *conn,