e87e36195197b5bb0b5fec62b0e2a46aecc5f947
Claudio Saavedra <csaavedra@igalia.com>
2018-06-07 10:38:17 +0300

    HSTS: Rewrite the HSTS feature and add tests
    This is a comprehensive rework of the HSTS enforcer and related
    classes, based upon Adrien Plazas work. A summary of the most
    relevant changes:
    
    SoupHSTSEnforcer:
    
    - The enforcer will listen on headers both on message queueing
      and restarting. This is necessary in order to be able to enforce
      HSTS redirections on messages that are restarted for whatever
      reason.
    - Instead of causing a redirection, the URI will be overwritten
      directly on the message before it is sent. Redirections
      are for use on the server side, and the tests added show that
      it is not a reliable way to do HSTS enforcing. Currently,
      the only way to find out that a HSTS policy has been enforced
      is by listening to the SoupMessage:uri property changes, but
      this might be impractical, so this could be revisited in the
      future.
    - soup_hsts_enforcer_policy() will not steal the given policy.
      Doing so is prone to leaks and not customary.
    - SoupHSTSEnforcerClass now has a has_valid_policy() vfunc.
      It currently works exactly as before, but the idea here is to
      make it possible for subclasses to implement their own check
      for existence of valid policies for domains, instead of all
      subclasses having to add their policies to the base
      SoupHSTSEnforcer class. This will be useful when having a large
      number of pre-loaded HSTS policies (either in SoupHSTSEnforcerDB
      or in an enforcer using libhsts as a backend) to avoid having
      potentially thousands of policies in memory at all times.
    - HSTS headers are parsed using soup's available utilities,
      instead of parsing them by hand. The specification is carefully
      followed so as to not accept any header that is not fully
      compliant.
    
    SoupHSTSEnforcerDB:
    
    - Store the max-age attribute in the database. This was done before
      errata 5372 was reported to RFC 6797, and its necessity will
      depend on how the errata is treated.
    
    Other:
    
    - Added tests for both enforcer classes that cover most of the
      specification.
    - Added the gtk-doc documentation and update all the documentation
      comments.
    - Rename SoupHsts classes to SoupHSTS for consistent naming and
      other minor renaming of parameters and methods.

--- libsoup/soup-hsts-policy.h
+++ libsoup/soup-hsts-policy.h
@@ -1,59 +1,61 @@
 /* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
 /*
- * Copyright (C) 2016 Igalia S.L.
+ * Copyright (C) 2016, 2017, 2018 Igalia S.L.
+ * Copyright (C) 2017, 2018 Metrological Group B.V.
  */
 
-#ifndef SOUP_HSTS_POLICY_H
-#define SOUP_HSTS_POLICY_H 1
+#ifndef __SOUP_HSTS_POLICY_H__
+#define __SOUP_HSTS_POLICY_H__ 1
 
 #include <libsoup/soup-types.h>
 
 G_BEGIN_DECLS
 
-struct _SoupHstsPolicy {
+struct _SoupHSTSPolicy {
 	char                 *domain;
+	unsigned long         max_age;
 	SoupDate             *expires;
-	gboolean              include_sub_domains;
+	gboolean              include_subdomains;
 };
 
-SOUP_AVAILABLE_IN_2_54
-GType soup_hsts_policy_get_type (void);
+SOUP_AVAILABLE_IN_2_66
+GType		 soup_hsts_policy_get_type (void);
 #define SOUP_TYPE_HSTS_POLICY (soup_hsts_policy_get_type())
 
 #define SOUP_HSTS_POLICY_MAX_AGE_PAST (0)
 
-SOUP_AVAILABLE_IN_2_54
-SoupHstsPolicy *soup_hsts_policy_new		(const char *domain,
-						 SoupDate   *expiry_date,
-						 gboolean    include_sub_domains);
-SOUP_AVAILABLE_IN_2_54
-SoupHstsPolicy *soup_hsts_policy_new_with_max_age	(const char *domain,
-							 int         max_age,
-							 gboolean    include_sub_domains);
-SOUP_AVAILABLE_IN_2_54
-SoupHstsPolicy *soup_hsts_policy_new_permanent		(const char *domain,
-							 gboolean    include_sub_domains);
-SOUP_AVAILABLE_IN_2_54
-SoupHstsPolicy *soup_hsts_policy_new_from_response	(SoupMessage *msg);
+SOUP_AVAILABLE_IN_2_66
+SoupHSTSPolicy  *soup_hsts_policy_new			(const char	*domain,
+							 unsigned long	 max_age,
+							 gboolean	 include_subdomains);
+SOUP_AVAILABLE_IN_2_66
+SoupHSTSPolicy  *soup_hsts_policy_new_full		(const char	*domain,
+							 unsigned long	 max_age,
+							 SoupDate	*expires,
+							 gboolean	 include_subdomains);
+SOUP_AVAILABLE_IN_2_66
+SoupHSTSPolicy  *soup_hsts_policy_new_session_policy	(const char	*domain,
+							 gboolean	 include_subdomains);
+SOUP_AVAILABLE_IN_2_66
+SoupHSTSPolicy  *soup_hsts_policy_new_from_response	(SoupMessage	*msg);
 
-SOUP_AVAILABLE_IN_2_54
-SoupHstsPolicy *soup_hsts_policy_copy           (SoupHstsPolicy *policy);
-SOUP_AVAILABLE_IN_2_54
-gboolean soup_hsts_policy_equal                 (SoupHstsPolicy *policy1,
-                                                 SoupHstsPolicy *policy2);
+SOUP_AVAILABLE_IN_2_66
+SoupHSTSPolicy  *soup_hsts_policy_copy			(SoupHSTSPolicy *policy);
+SOUP_AVAILABLE_IN_2_66
+gboolean	 soup_hsts_policy_equal			(SoupHSTSPolicy *policy1,
+							 SoupHSTSPolicy *policy2);
+SOUP_AVAILABLE_IN_2_66
+const char	*soup_hsts_policy_get_domain		(SoupHSTSPolicy *policy);
+SOUP_AVAILABLE_IN_2_66
+gboolean	 soup_hsts_policy_is_expired		(SoupHSTSPolicy *policy);
+SOUP_AVAILABLE_IN_2_66
+gboolean	 soup_hsts_policy_includes_subdomains 	(SoupHSTSPolicy *policy);
+SOUP_AVAILABLE_IN_2_66
+gboolean	 soup_hsts_policy_is_session_policy	(SoupHSTSPolicy *policy);
 
-SOUP_AVAILABLE_IN_2_54
-const char *soup_hsts_policy_get_domain         (SoupHstsPolicy *policy);
-SOUP_AVAILABLE_IN_2_54
-gboolean    soup_hsts_policy_is_expired         (SoupHstsPolicy *policy);
-SOUP_AVAILABLE_IN_2_54
-gboolean    soup_hsts_policy_includes_sub_domains       (SoupHstsPolicy *policy);
-SOUP_AVAILABLE_IN_2_54
-gboolean    soup_hsts_policy_is_permanent       (SoupHstsPolicy *policy);
-
-SOUP_AVAILABLE_IN_2_54
-void        soup_hsts_policy_free               (SoupHstsPolicy *policy);
+SOUP_AVAILABLE_IN_2_66
+void		 soup_hsts_policy_free			(SoupHSTSPolicy *policy);
 
 G_END_DECLS
 
-#endif /* SOUP_HSTS_POLICY_H */
+#endif /* __SOUP_HSTS_POLICY_H__ */
