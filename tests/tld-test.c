9604b88138e3083437f586f6abfd19371c964ddf
Claudio Saavedra <csaavedra@igalia.com>
2018-10-22 19:05:49 +0300

    tld-test: update after changes in the public-suffix list
    *.bn was removed from the list, so the tests using it started failing.
    Use *.bd instead as a 1-wildcard rule test domain.

--- tests/tld-test.c
+++ tests/tld-test.c
@@ -41,10 +41,10 @@ static struct {
 	{ "a.b.example.uk.com", "example.uk.com", -1 },
 	{ "test.ac", "test.ac", -1 },
 	/* TLD with only 1 (wildcard) rule. */
-	{ "bn", NULL, SOUP_TLD_ERROR_NOT_ENOUGH_DOMAINS },
-	{ "c.bn", NULL, SOUP_TLD_ERROR_NOT_ENOUGH_DOMAINS },
-	{ "b.c.bn", "b.c.bn", -1 },
-	{ "a.b.c.bn", "b.c.bn", -1 },
+	{ "bd", NULL, SOUP_TLD_ERROR_NOT_ENOUGH_DOMAINS },
+	{ "c.bd", NULL, SOUP_TLD_ERROR_NOT_ENOUGH_DOMAINS },
+	{ "b.c.bd", "b.c.bd", -1 },
+	{ "a.b.c.bd", "b.c.bd", -1 },
 	/* More complex TLD. */
 	{ "jp", NULL, SOUP_TLD_ERROR_NOT_ENOUGH_DOMAINS },
 	{ "test.jp", "test.jp", -1 },
