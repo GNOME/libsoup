bf667ff601a9172955d5e8a8f08480e9e1477937
Xavier Claessens <xavier.claessens@collabora.com>
2018-12-10 21:17:13 -0500

    ci: Add Dockerfile
    It is a waste of time to reinstall all fedora packages for each job.
    Copy and adapt Dockerfile and scripts from glib.

---
+++ .gitlab-ci/run-docker.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+TAG="registry.gitlab.gnome.org/gnome/libsoup/master:v1"
+
+cd "$(dirname "$0")"
+docker build --build-arg HOST_USER_ID="$UID" --tag "${TAG}" \
+    --file "Dockerfile" .
+
+if [ "$1" = "--push" ]; then
+  docker login registry.gitlab.gnome.org
+  docker push $TAG
+else
+  docker run --rm \
+      --volume "$(pwd)/..:/home/user/app" --workdir "/home/user/app" \
+      --tty --interactive "${TAG}" bash
+fi
