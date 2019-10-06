b4f6d4cc9bdd5d214dbf7d451849ac89c3ad79cd
Simon McVittie <smcv@debian.org>
2019-10-06 16:44:15 +0100

    xmlrpc tests: Cope with GLib 2.62 TAP output
    In GLib 2.62, "some-test-executable -l" outputs TAP headers before the
    list of tests. Ignore those lines.
    
    Signed-off-by: Simon McVittie <smcv@debian.org>

--- tests/xmlrpc-server-test.c
+++ tests/xmlrpc-server-test.c
@@ -344,6 +344,12 @@ main (int argc, char **argv)
 		g_free (out);
 
 		for (i = 0; tests[i] && *tests[i]; i++) {
+			if (tests[i][0] == '#') {
+				continue;
+			}
+			if (g_str_has_prefix (tests[i], "1..")) {
+				continue;
+			}
 			g_assert_true (g_str_has_prefix (tests[i], "/xmlrpc/"));
 			path = g_strdup_printf ("/xmlrpc-server/%s", tests[i] + strlen ("/xmlrpc/"));
 			g_test_add_data_func (path, tests[i], do_one_xmlrpc_test);
