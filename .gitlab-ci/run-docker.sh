9004be5484db37dd26458ecb070e14309e2d777a
Philip Withnall <withnall@endlessm.com>
2020-07-27 14:44:31 +0100

    ci: Fix incompatibility between docker and podman image formats
    podman provides a `docker` command and claims to interoperate with it,
    but then silently builds OCI images in a format which is incompatible
    with some `dockerd` versions, which run on some of the GNOME GitLab CI
    runners. Sigh.
    
    Signed-off-by: Philip Withnall <withnall@endlessm.com>

--- .gitlab-ci/run-docker.sh
+++ .gitlab-ci/run-docker.sh
@@ -4,15 +4,25 @@ set -e
 
 TAG="registry.gitlab.gnome.org/gnome/libsoup/master:v3"
 
+SUDO_CMD="sudo"
+if docker -v |& grep -q podman; then
+        # Using podman
+        SUDO_CMD=""
+        # Docker is actually implemented by podman, and its OCI output
+        # is incompatible with some of the dockerd instances on GitLab
+        # CI runners.
+        export BUILDAH_FORMAT=docker
+fi
+
 cd "$(dirname "$0")"
-docker build --build-arg HOST_USER_ID="$UID" --tag "${TAG}" \
+$SUDO_CMD docker build --build-arg HOST_USER_ID="$UID" --tag "${TAG}" \
     --file "Dockerfile" .
 
 if [ "$1" = "--push" ]; then
-  docker login registry.gitlab.gnome.org
-  docker push $TAG
+  $SUDO_CMD docker login registry.gitlab.gnome.org
+  $SUDO_CMD docker push $TAG
 else
-  docker run --rm \
+  $SUDO_CMD docker run --rm \
       --volume "$(pwd)/..:/home/user/app" --workdir "/home/user/app" \
       --tty --interactive "${TAG}" bash
 fi
