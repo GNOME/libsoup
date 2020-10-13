93003fce0e04f09fb405f982b0a2a91c1fd299b9
Patrick Griffis <pgriffis@igalia.com>
2020-10-13 13:53:08 -0500

    ci: Use podman to generate images

--- .gitlab-ci/run-docker.sh
+++ .gitlab-ci/run-docker.sh
@@ -4,25 +4,14 @@ set -e
 
 TAG="registry.gitlab.gnome.org/gnome/libsoup/master:v5"
 
-SUDO_CMD="sudo"
-if docker -v |& grep -q podman; then
-        # Using podman
-        SUDO_CMD=""
-        # Docker is actually implemented by podman, and its OCI output
-        # is incompatible with some of the dockerd instances on GitLab
-        # CI runners.
-        export BUILDAH_FORMAT=docker
-fi
-
 cd "$(dirname "$0")"
-$SUDO_CMD docker build --build-arg HOST_USER_ID="$UID" --tag "${TAG}" \
-    --file "Dockerfile" .
+podman build --build-arg HOST_USER_ID="$UID" --tag "${TAG}" --file "Dockerfile" --format=docker .
 
 if [ "$1" = "--push" ]; then
-  $SUDO_CMD docker login registry.gitlab.gnome.org
-  $SUDO_CMD docker push $TAG
+  podman login registry.gitlab.gnome.org
+  podman push $TAG
 else
-  $SUDO_CMD docker run --rm \
+  podman run --rm \
       --volume "$(pwd)/..:/home/user/app" --workdir "/home/user/app" \
       --tty --interactive "${TAG}" bash
 fi
