2c9f6df68adb7f0c29f6661195ac661f7f60402b
Diego Pino Garcia <dpino@igalia.com>
2021-01-21 11:41:52 +0800

    tests: add full license in tests/autobahn/soup-autobahn-test-client.c

--- tests/autobahn/soup-autobahn-test-client.c
+++ tests/autobahn/soup-autobahn-test-client.c
@@ -1,6 +1,23 @@
 /* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
 /*
- * Copyright (C) 2019 Igalia S.L.
+ * soup-autobahn-test-client.c
+ *
+ * Copyright (C) 2021 Igalia S.L.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: LGPL-2.0-or-later
  */
 
 #include <stdio.h>
