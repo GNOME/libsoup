e61b85db27a937aa60bc23c5aa393b7eed0887ce
Carlos Garcia Campos <cgarcia@igalia.com>
2021-03-26 09:50:41 +0100

    docs: fix documentation errors

--- libsoup/soup-status.c
+++ libsoup/soup-status.c
@@ -251,6 +251,7 @@ soup_status_get_phrase (guint status_code)
  * SoupHTTPVersion:
  * @SOUP_HTTP_1_0: HTTP 1.0 (RFC 1945)
  * @SOUP_HTTP_1_1: HTTP 1.1 (RFC 2616)
+ * @SOUP_HTTP_2_0: HTTP 2.0 (RFC 7540)
  *
  * Indicates the HTTP protocol version being used.
  */
