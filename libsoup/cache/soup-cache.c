GNOME/libsoup

commit 4596b4ec4c14a0e473e3fa0c3f41a0107d846cc8

Replace SoupDate with GDateTime

libsoup/cache/soup-cache.c
@@ -390,18 +390,18 @@ soup_cache_entry_set_freshness (SoupCacheEntry *entry, SoupMessage *msg, SoupCac
 	expires = soup_message_headers_get_one (entry->headers, "Expires");
 	date = soup_message_headers_get_one (entry->headers, "Date");
 	if (expires && date) {
-		SoupDate *expires_d, *date_d;
+		GDateTime *expires_d, *date_d;
 		time_t expires_t, date_t;
 
-		expires_d = soup_date_new_from_string (expires);
+		expires_d = soup_date_time_new_from_http_string (expires);
 		if (expires_d) {
-			date_d = soup_date_new_from_string (date);
+			date_d = soup_date_time_new_from_http_string (date);
 
-			expires_t = soup_date_to_time_t (expires_d);
-			date_t = soup_date_to_time_t (date_d);
+			expires_t = g_date_time_to_unix (expires_d);
+			date_t = g_date_time_to_unix (date_d);
 
-			soup_date_free (expires_d);
-			soup_date_free (date_d);
+			g_date_time_unref (expires_d);
+			g_date_time_unref (date_d);
 
 			if (expires_t && date_t) {
 				entry->freshness_lifetime = (guint32) MAX (expires_t - date_t, 0);
@@ -434,17 +434,17 @@ soup_cache_entry_set_freshness (SoupCacheEntry *entry, SoupMessage *msg, SoupCac
 	/* Last-Modified based heuristic */
 	last_modified = soup_message_headers_get_one (entry->headers, "Last-Modified");
 	if (last_modified) {
-		SoupDate *soup_date;
+		GDateTime *soup_date;
 		time_t now, last_modified_t;
 
-		soup_date = soup_date_new_from_string (last_modified);
-		last_modified_t = soup_date_to_time_t (soup_date);
+		soup_date = soup_date_time_new_from_http_string (last_modified);
+		last_modified_t = g_date_time_to_unix (soup_date);
 		now = time (NULL);
 
 #define HEURISTIC_FACTOR 0.1 /* From Section 2.3.1.1 */
 
 		entry->freshness_lifetime = MAX (0, (now - last_modified_t) * HEURISTIC_FACTOR);
-		soup_date_free (soup_date);
+		g_date_time_unref (soup_date);
 	}
 
 	return;
@@ -481,13 +481,13 @@ soup_cache_entry_new (SoupCache *cache, SoupMessage *msg, time_t request_time, t
 	date = soup_message_headers_get_one (entry->headers, "Date");
 
 	if (date) {
-		SoupDate *soup_date;
+		GDateTime *soup_date;
 		const char *age;
 		time_t date_value, apparent_age, corrected_received_age, response_delay, age_value = 0;
 
-		soup_date = soup_date_new_from_string (date);
-		date_value = soup_date_to_time_t (soup_date);
-		soup_date_free (soup_date);
+		soup_date = soup_date_time_new_from_http_string (date);
+		date_value = g_date_time_to_unix (soup_date);
+		g_date_time_unref (soup_date);
 
 		age = soup_message_headers_get_one (entry->headers, "Age");
 		if (age)

libsoup/cookies/soup-cookie-jar-db.c
@@ -297,7 +297,7 @@ soup_cookie_jar_db_changed (SoupCookieJar *jar,
 	if (new_cookie && new_cookie->expires) {
 		gulong expires;
 		
-		expires = (gulong)soup_date_to_time_t (new_cookie->expires);
+		expires = (gulong)g_date_time_to_unix (new_cookie->expires);
 		query = sqlite3_mprintf (QUERY_INSERT, 
 					 new_cookie->name,
 					 new_cookie->value,

libsoup/cookies/soup-cookie-jar-text.c
@@ -267,7 +267,7 @@ write_cookie (FILE *out, SoupCookie *cookie)
 		 *cookie->domain == '.' ? "TRUE" : "FALSE",
 		 cookie->path,
 		 cookie->secure ? "TRUE" : "FALSE",
-		 (gulong)soup_date_to_time_t (cookie->expires),
+		 (gulong)g_date_time_to_unix (cookie->expires),
 		 cookie->name,
 		 cookie->value,
 		 same_site_policy_to_string (soup_cookie_get_same_site_policy (cookie)));

libsoup/cookies/soup-cookie-jar.c
@@ -12,6 +12,7 @@
 #include <string.h>
 
 #include "soup-cookie-jar.h"
+#include "soup-date-utils-private.h"
 #include "soup-message-private.h"
 #include "soup-misc.h"
 #include "soup.h"
@@ -343,7 +344,7 @@ get_cookies (SoupCookieJar *jar,
 			GSList *next = domain_cookies->next;
 			SoupCookie *cookie = domain_cookies->data;
 
-			if (cookie->expires && soup_date_is_past (cookie->expires)) {
+			if (cookie->expires && soup_date_time_is_past (cookie->expires)) {
 				cookies_to_remove = g_slist_append (cookies_to_remove,
 								    cookie);
 				new_head = g_slist_delete_link (new_head, domain_cookies);
@@ -610,7 +611,7 @@ soup_cookie_jar_add_cookie_full (SoupCookieJar *jar, SoupCookie *cookie, SoupURI
 				 * https://tools.ietf.org/html/draft-ietf-httpbis-cookie-alone-01
 				 */
 				soup_cookie_free (cookie);
-			} else if (cookie->expires && soup_date_is_past (cookie->expires)) {
+			} else if (cookie->expires && soup_date_time_is_past (cookie->expires)) {
 				/* The new cookie has an expired date,
 				 * this is the way the the server has
 				 * of telling us that we have to
@@ -635,7 +636,7 @@ soup_cookie_jar_add_cookie_full (SoupCookieJar *jar, SoupCookie *cookie, SoupURI
 	}
 
 	/* The new cookie is... a new cookie */
-	if (cookie->expires && soup_date_is_past (cookie->expires)) {
+	if (cookie->expires && soup_date_time_is_past (cookie->expires)) {
 		soup_cookie_free (cookie);
 		return;
 	}

libsoup/cookies/soup-cookie.c
@@ -13,6 +13,7 @@
 #include <string.h>
 
 #include "soup-cookie.h"
+#include "soup-date-utils-private.h"
 #include "soup-misc.h"
 #include "soup.h"
 
@@ -85,7 +86,7 @@ soup_cookie_copy (SoupCookie *cookie)
 	copy->domain = g_strdup (cookie->domain);
 	copy->path = g_strdup (cookie->path);
 	if (cookie->expires)
-		copy->expires = soup_date_copy(cookie->expires);
+		copy->expires = g_date_time_ref (cookie->expires);
 	copy->secure = cookie->secure;
 	copy->http_only = cookie->http_only;
 	soup_cookie_set_same_site_policy (copy, soup_cookie_get_same_site_policy (cookie));
@@ -157,14 +158,14 @@ parse_value (const char **val_p, gboolean copy)
 	return value;
 }
 
-static SoupDate *
+static GDateTime *
 parse_date (const char **val_p)
 {
 	char *value;
-	SoupDate *date;
+	GDateTime *date;
 
 	value = parse_value (val_p, TRUE);
-	date = soup_date_new_from_string (value);
+	date = soup_date_time_new_from_http_string (value);
 	g_free (value);
 	return date;
 }
@@ -563,7 +564,7 @@ void
 soup_cookie_set_max_age (SoupCookie *cookie, int max_age)
 {
 	if (cookie->expires)
-		soup_date_free (cookie->expires);
+		g_date_time_unref (cookie->expires);
 
 	if (max_age == -1)
 		cookie->expires = NULL;
@@ -571,9 +572,12 @@ soup_cookie_set_max_age (SoupCookie *cookie, int max_age)
 		/* Use a date way in the past, to protect against
 		 * clock skew.
 		 */
-		cookie->expires = soup_date_new (1970, 1, 1, 0, 0, 0);
-	} else
-		cookie->expires = soup_date_new_from_now (max_age);
+		cookie->expires = g_date_time_new_from_unix_utc (0);
+	} else {
+                GDateTime *now = g_date_time_new_now_utc ();
+                cookie->expires = g_date_time_add_seconds (now, max_age);
+                g_date_time_unref (now);
+        }
 }
 
 /**
@@ -611,7 +615,7 @@ soup_cookie_set_max_age (SoupCookie *cookie, int max_age)
 
 /**
  * soup_cookie_get_expires:
- * @cookie: a #SoupCookie
+ * @cookie: a #GDateTime
  *
  * Gets @cookie's expiration time.
  *
@@ -621,7 +625,7 @@ soup_cookie_set_max_age (SoupCookie *cookie, int max_age)
  *
  * Since: 2.32
  **/
-SoupDate *
+GDateTime *
 soup_cookie_get_expires (SoupCookie *cookie)
 {
 	return cookie->expires;
@@ -641,13 +645,13 @@ soup_cookie_get_expires (SoupCookie *cookie)
  * Since: 2.24
  **/
 void
-soup_cookie_set_expires (SoupCookie *cookie, SoupDate *expires)
+soup_cookie_set_expires (SoupCookie *cookie, GDateTime *expires)
 {
 	if (cookie->expires)
-		soup_date_free (cookie->expires);
+		g_date_time_unref (cookie->expires);
 
 	if (expires)
-		cookie->expires = soup_date_copy (expires);
+		cookie->expires = g_date_time_ref (expires);
 	else
 		cookie->expires = NULL;
 }
@@ -745,8 +749,8 @@ serialize_cookie (SoupCookie *cookie, GString *header, gboolean set_cookie)
 		char *timestamp;
 
 		g_string_append (header, "; expires=");
-		timestamp = soup_date_to_string (cookie->expires,
-						 SOUP_DATE_COOKIE);
+		timestamp = soup_date_time_to_string (cookie->expires,
+						      SOUP_DATE_COOKIE);
 		g_string_append (header, timestamp);
 		g_free (timestamp);
 	}
@@ -872,7 +876,7 @@ soup_cookie_free (SoupCookie *cookie)
 	g_free (cookie->value);
 	g_free (cookie->domain);
 	g_free (cookie->path);
-	g_clear_pointer (&cookie->expires, soup_date_free);
+	g_clear_pointer (&cookie->expires, g_date_time_unref);
 
 	g_dataset_destroy (cookie);
 	g_slice_free (SoupCookie, cookie);
@@ -1085,7 +1089,7 @@ soup_cookie_applies_to_uri (SoupCookie *cookie, SoupURI *uri)
 	if (cookie->secure && !soup_uri_is_https (uri, NULL))
 		return FALSE;
 
-	if (cookie->expires && soup_date_is_past (cookie->expires))
+	if (cookie->expires && soup_date_time_is_past (cookie->expires))
 		return FALSE;
 
 	/* uri->path is required to be non-NULL */

libsoup/cookies/soup-cookie.h
@@ -24,13 +24,13 @@ typedef enum {
 } SoupSameSitePolicy;
 
 struct _SoupCookie {
-	char     *name;
-	char     *value;
-	char     *domain;
-	char     *path;
-	SoupDate *expires;
-	gboolean  secure;
-	gboolean  http_only;
+	char      *name;
+	char      *value;
+	char      *domain;
+	char      *path;
+	GDateTime *expires;
+	gboolean   secure;
+	gboolean   http_only;
 };
 
 SOUP_AVAILABLE_IN_2_24
@@ -78,10 +78,10 @@ SOUP_AVAILABLE_IN_2_24
 void        soup_cookie_set_max_age             (SoupCookie  *cookie,
 						 int          max_age);
 SOUP_AVAILABLE_IN_2_32
-SoupDate   *soup_cookie_get_expires             (SoupCookie  *cookie);
+GDateTime   *soup_cookie_get_expires            (SoupCookie  *cookie);
 SOUP_AVAILABLE_IN_2_24
 void        soup_cookie_set_expires             (SoupCookie  *cookie,
-						 SoupDate    *expires);
+						 GDateTime    *expires);
 SOUP_AVAILABLE_IN_2_32
 gboolean    soup_cookie_get_secure              (SoupCookie  *cookie);
 SOUP_AVAILABLE_IN_2_24

libsoup/hsts/soup-hsts-enforcer-db.c
@@ -148,7 +148,7 @@ query_all_callback (void *data, int argc, char **argv, char **colname)
 	gulong expire_time;
 	unsigned long max_age;
 	time_t now;
-	SoupDate *expires;
+	GDateTime *expires;
 	gboolean include_subdomains = FALSE;
 
 	now = time (NULL);
@@ -159,7 +159,7 @@ query_all_callback (void *data, int argc, char **argv, char **colname)
 	if (now >= expire_time)
 		return 0;
 
-	expires = soup_date_new_from_time_t (expire_time);
+	expires = g_date_time_new_from_unix_utc (expire_time);
 	max_age = strtoul (argv[COL_MAX_AGE], NULL, 10);
 	include_subdomains = (g_strcmp0 (argv[COL_SUBDOMAINS], "1") == 0);
 
@@ -169,7 +169,7 @@ query_all_callback (void *data, int argc, char **argv, char **colname)
 		soup_hsts_enforcer_set_policy (hsts_enforcer, policy);
 		soup_hsts_policy_free (policy);
 	} else
-		soup_date_free (expires);
+		g_date_time_unref (expires);
 
 	return 0;
 }
@@ -277,7 +277,7 @@ soup_hsts_enforcer_db_changed (SoupHSTSEnforcer *hsts_enforcer,
 	if (new_policy && new_policy->expires) {
 		gulong expires;
 
-		expires = (gulong)soup_date_to_time_t (new_policy->expires);
+		expires = (gulong)g_date_time_to_unix (new_policy->expires);
 		query = sqlite3_mprintf (QUERY_INSERT,
 					 new_policy->domain,
 					 new_policy->domain,

libsoup/hsts/soup-hsts-policy.c
@@ -15,6 +15,7 @@
 #include <string.h>
 
 #include "soup-hsts-policy.h"
+#include "soup-date-utils-private.h"
 #include "soup.h"
 
 /**
@@ -76,7 +77,7 @@ soup_hsts_policy_copy (SoupHSTSPolicy *policy)
 	copy->domain = g_strdup (policy->domain);
 	copy->max_age = policy->max_age;
 	copy->expires = policy->expires ?
-		soup_date_copy (policy->expires) : NULL;
+		g_date_time_ref (policy->expires) : NULL;
 	copy->include_subdomains = policy->include_subdomains;
 
 	return copy;
@@ -113,8 +114,7 @@ soup_hsts_policy_equal (SoupHSTSPolicy *policy1, SoupHSTSPolicy *policy2)
 		return FALSE;
 
 	if (policy1->expires && policy2->expires &&
-	    soup_date_to_time_t (policy1->expires) !=
-	    soup_date_to_time_t (policy2->expires))
+	    !g_date_time_equal (policy1->expires, policy2->expires))
 		return FALSE;
 
 	return TRUE;
@@ -158,15 +158,18 @@ soup_hsts_policy_new (const char *domain,
 		      unsigned long max_age,
 		      gboolean include_subdomains)
 {
-	SoupDate *expires;
+	GDateTime *expires;
 
 	if (max_age == SOUP_HSTS_POLICY_MAX_AGE_PAST) {
 		/* Use a date way in the past, to protect against
 		 * clock skew.
 		 */
-		expires = soup_date_new (1970, 1, 1, 0, 0, 0);
-	} else
-		expires = soup_date_new_from_now (max_age);
+                expires = g_date_time_new_from_unix_utc (0);
+	} else {
+                GDateTime *now = g_date_time_new_now_utc ();
+                expires = g_date_time_add_seconds (now, max_age);
+                g_date_time_unref (now);
+        }
 
 	return soup_hsts_policy_new_full (domain, max_age, expires, include_subdomains);
 }
@@ -188,7 +191,7 @@ soup_hsts_policy_new (const char *domain,
 SoupHSTSPolicy *
 soup_hsts_policy_new_full (const char *domain,
 			   unsigned long max_age,
-			   SoupDate *expires,
+			   GDateTime *expires,
 			   gboolean include_subdomains)
 {
 	SoupHSTSPolicy *policy;
@@ -208,7 +211,7 @@ soup_hsts_policy_new_full (const char *domain,
 	}
 
 	policy->max_age = max_age;
-	policy->expires = expires;
+	policy->expires = expires ? g_date_time_ref (expires) : NULL;
 	policy->include_subdomains = include_subdomains;
 
 	return policy;
@@ -345,7 +348,7 @@ soup_hsts_policy_is_expired (SoupHSTSPolicy *policy)
 {
 	g_return_val_if_fail (policy != NULL, TRUE);
 
-	return policy->expires && soup_date_is_past (policy->expires);
+	return policy->expires && soup_date_time_is_past (policy->expires);
 }
 
 /**
@@ -399,6 +402,6 @@ soup_hsts_policy_free (SoupHSTSPolicy *policy)
 	g_return_if_fail (policy != NULL);
 
 	g_free (policy->domain);
-	g_clear_pointer (&policy->expires, soup_date_free);
+	g_clear_pointer (&policy->expires, g_date_time_unref);
 	g_slice_free (SoupHSTSPolicy, policy);
 }

commit 32a079c2bf10c99075eac0cab5425fbe31148ba4

cache: Use gint64 to handle time GDateTime returns gint64 for unix timestamps instead of time_t as it can store larger values. This was a regression introduced in 4596b4ec. Fixes #236

libsoup/cache/soup-cache.c
@@ -412,7 +412,7 @@ soup_cache_entry_set_freshness (SoupCacheEntry *entry, SoupMessage *msg, SoupCac
 	date = soup_message_headers_get_one_common (entry->headers, SOUP_HEADER_DATE);
 	if (expires && date) {
 		GDateTime *expires_d, *date_d;
-		time_t expires_t, date_t;
+		gint64 expires_t, date_t;
 
 		expires_d = soup_date_time_new_from_http_string (expires);
 		if (expires_d) {
@@ -456,7 +456,7 @@ soup_cache_entry_set_freshness (SoupCacheEntry *entry, SoupMessage *msg, SoupCac
 	last_modified = soup_message_headers_get_one_common (entry->headers, SOUP_HEADER_LAST_MODIFIED);
 	if (last_modified) {
 		GDateTime *soup_date;
-		time_t now, last_modified_t;
+		gint64 now, last_modified_t;
 
 		soup_date = soup_date_time_new_from_http_string (last_modified);
 		last_modified_t = g_date_time_to_unix (soup_date);
@@ -504,7 +504,7 @@ soup_cache_entry_new (SoupCache *cache, SoupMessage *msg, time_t request_time, t
 	if (date) {
 		GDateTime *soup_date;
 		const char *age;
-		time_t date_value, apparent_age, corrected_received_age, response_delay, age_value = 0;
+		gint64 date_value, apparent_age, corrected_received_age, response_delay, age_value = 0;
 
 		soup_date = soup_date_time_new_from_http_string (date);
 		date_value = g_date_time_to_unix (soup_date);