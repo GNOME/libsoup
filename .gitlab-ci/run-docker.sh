c5e383fb8e974b3d0738ffbb5cf75d811c67fd9d
Patrick Griffis <pgriffis@igalia.com>
2022-01-16 14:49:08 -0600

    docker: Add gi-docgen

--- .gitlab-ci/run-docker.sh
+++ .gitlab-ci/run-docker.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-TAG="registry.gitlab.gnome.org/gnome/libsoup/master:v15"
+TAG="registry.gitlab.gnome.org/gnome/libsoup/master:v16"
 
 SUDO_CMD="sudo"
 if docker -v |& grep -q podman; then
