63cd39f18d3d815a89ea3eecfcae056f80888e9f
Emmanuele Bassi <ebassi@gnome.org>
2022-08-15 00:38:38 +0100

    Use the project deprecation macros
    This way we ensure every symbol gets exported, even when deprecated, and
    that deprecation warnings can be disabled.

--- libsoup/generate-version-header.py
+++ libsoup/generate-version-header.py
@@ -50,11 +50,11 @@ for version in versions:
  * A macro used to indicate a symbol was deprecated in this version with a replacement.
  */
 #if SOUP_VERSION_MIN_REQUIRED >= SOUP_VERSION_{major_version}_{minor_version}
-# define SOUP_DEPRECATED_IN_{major_version}_{minor_version}                G_DEPRECATED
-# define SOUP_DEPRECATED_IN_{major_version}_{minor_version}_FOR(f)         G_DEPRECATED_FOR(f)
+# define SOUP_DEPRECATED_IN_{major_version}_{minor_version}                SOUP_DEPRECATED
+# define SOUP_DEPRECATED_IN_{major_version}_{minor_version}_FOR(f)         SOUP_DEPRECATED_FOR(f)
 #else
-# define SOUP_DEPRECATED_IN_{major_version}_{minor_version}
-# define SOUP_DEPRECATED_IN_{major_version}_{minor_version}_FOR(f)
+# define SOUP_DEPRECATED_IN_{major_version}_{minor_version}                _SOUP_EXTERN
+# define SOUP_DEPRECATED_IN_{major_version}_{minor_version}_FOR(f)         _SOUP_EXTERN
 #endif
 
 /**
@@ -62,7 +62,7 @@ for version in versions:
  * A macro used to indicate a symbol is available in this version or later.
  */
 #if SOUP_VERSION_MAX_ALLOWED < SOUP_VERSION_{major_version}_{minor_version}
-# define SOUP_AVAILABLE_IN_{major_version}_{minor_version}                 G_UNAVAILABLE({major_version}, {minor_version}) _SOUP_EXTERN
+# define SOUP_AVAILABLE_IN_{major_version}_{minor_version}                 SOUP_UNAVAILABLE({major_version}, {minor_version}) _SOUP_EXTERN
 #else
 # define SOUP_AVAILABLE_IN_{major_version}_{minor_version}                 _SOUP_EXTERN
 #endif
