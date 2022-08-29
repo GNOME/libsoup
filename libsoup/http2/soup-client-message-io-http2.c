1b60814b152c973d3a71b1ef9e149b6632a9ecd0
Carlos Garcia Campos <cgarcia@igalia.com>
2022-08-29 11:35:38 +0200

    http2: mark IO as shutting down before processing async pending messages

--- libsoup/http2/soup-client-message-io-http2.c
+++ libsoup/http2/soup-client-message-io-http2.c
@@ -415,14 +415,14 @@ io_read_ready (GObject                  *stream,
                 return G_SOURCE_CONTINUE;
         }
 
+        io->is_shutdown = TRUE;
+
         if (error) {
                 set_io_error (io, error);
                 g_list_foreach (io->pending_io_messages,
                                 (GFunc)soup_http2_message_data_check_status,
                                 NULL);
         }
-
-        io->is_shutdown = TRUE;
 
         g_clear_pointer (&io->read_source, g_source_unref);
         if (conn) {
