a3741a472337dea80aa8b9e76436597b08dbe6a0
Patrick Griffis <pgriffis@igalia.com>
2022-10-06 13:18:08 -0500

    ci: Update to Fedora 36

--- .gitlab-ci/run-docker.sh
+++ .gitlab-ci/run-docker.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-TAG="registry.gitlab.gnome.org/gnome/libsoup/master:v17"
+TAG="registry.gitlab.gnome.org/gnome/libsoup/master:v18"
 
 SUDO_CMD="sudo"
 if docker -v |& grep -q podman; then
