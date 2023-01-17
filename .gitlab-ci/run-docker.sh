493e04bb007db1f0af5416e33f3912261e8afde6
Patrick Griffis <pgriffis@igalia.com>
2023-01-16 19:17:07 -0600

    CI: Generate coverage reports
    Closes #290

--- .gitlab-ci/run-docker.sh
+++ .gitlab-ci/run-docker.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-TAG="registry.gitlab.gnome.org/gnome/libsoup/master:v19"
+TAG="registry.gitlab.gnome.org/gnome/libsoup/master:v20"
 
 SUDO_CMD="sudo"
 if docker -v |& grep -q podman; then
