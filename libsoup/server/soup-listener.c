GNOME/libsoup

commit 25e96cad913cde650ca56ff76c3924fd97eb7125

server: split SoupSocket into SoupListener and SoupServerConnection

libsoup/server/soup-listener.c
@@ -1,0 +1,378 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 8 -*- */
+/*
+ * soup-listener.c: Socket listening networking code.
+ *
+ * Copyright (C) 2022 Igalia S.L.
+ * Copyright (C) 2000-2003, Ximian, Inc.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <string.h>
+
+#include <glib/gi18n-lib.h>
+#include <gio/gnetworking.h>
+
+#include "soup-listener.h"
+#include "soup.h"
+#include "soup-io-stream.h"
+#include "soup-server-connection.h"
+
+enum {
+        NEW_CONNECTION,
+        LAST_SIGNAL
+};
+
+static guint signals[LAST_SIGNAL] = { 0 };
+
+enum {
+        PROP_0,
+
+        PROP_SOCKET,
+        PROP_TLS_CERTIFICATE,
+        PROP_TLS_DATABASE,
+        PROP_TLS_AUTH_MODE,
+
+        LAST_PROPERTY
+};
+
+static GParamSpec *properties[LAST_PROPERTY] = { NULL, };
+
+struct _SoupListener {
+        GObject parent_instance;
+};
+
+typedef struct {
+        GSocket *socket;
+        GIOStream *conn;
+        GIOStream *iostream;
+        GInetSocketAddress *local_addr;
+
+        GTlsCertificate *tls_certificate;
+        GTlsDatabase *tls_database;
+        GTlsAuthenticationMode tls_auth_mode;
+
+        GSource *source;
+} SoupListenerPrivate;
+
+G_DEFINE_FINAL_TYPE_WITH_PRIVATE (SoupListener, soup_listener, G_TYPE_OBJECT)
+
+static void
+soup_listener_init (SoupListener *listener)
+{
+}
+
+static gboolean
+listen_watch (GObject      *pollable,
+              SoupListener *listener)
+{
+        SoupListenerPrivate *priv = soup_listener_get_instance_private (listener);
+        GSocket *socket;
+        SoupServerConnection *conn;
+
+        socket = g_socket_accept (priv->socket, NULL, NULL);
+        if (!socket)
+                return G_SOURCE_REMOVE;
+
+        conn = soup_server_connection_new (socket, priv->tls_certificate, priv->tls_database, priv->tls_auth_mode);
+        g_signal_emit (listener, signals[NEW_CONNECTION], 0, conn);
+        g_object_unref (conn);
+
+        return G_SOURCE_CONTINUE;
+}
+
+static void
+soup_listener_constructed (GObject *object)
+{
+        SoupListener *listener = SOUP_LISTENER (object);
+        SoupListenerPrivate *priv = soup_listener_get_instance_private (listener);
+
+        g_socket_set_option (priv->socket, IPPROTO_TCP, TCP_NODELAY, TRUE, NULL);
+
+        priv->conn = (GIOStream *)g_socket_connection_factory_create_connection (priv->socket);
+        priv->iostream = soup_io_stream_new (priv->conn, FALSE);
+        priv->source = g_pollable_input_stream_create_source (G_POLLABLE_INPUT_STREAM (g_io_stream_get_input_stream (priv->iostream)), NULL);
+        g_source_set_callback (priv->source, (GSourceFunc)listen_watch, listener, NULL);
+        g_source_attach (priv->source, g_main_context_get_thread_default ());
+
+        G_OBJECT_CLASS (soup_listener_parent_class)->constructed (object);
+}
+
+static void
+soup_listener_finalize (GObject *object)
+{
+        SoupListener *listener = SOUP_LISTENER (object);
+        SoupListenerPrivate *priv = soup_listener_get_instance_private (listener);
+
+        if (priv->conn) {
+                g_io_stream_close (priv->conn, NULL, NULL);
+                g_clear_object (&priv->conn);
+        }
+
+        g_clear_object (&priv->socket);
+        g_clear_object (&priv->iostream);
+
+        g_clear_object (&priv->tls_certificate);
+        g_clear_object (&priv->tls_database);
+
+        if (priv->source) {
+                g_source_destroy (priv->source);
+                g_source_unref (priv->source);
+        }
+
+        G_OBJECT_CLASS (soup_listener_parent_class)->finalize (object);
+}
+
+static void
+soup_listener_set_property (GObject      *object,
+                            guint         prop_id,
+                            const GValue *value,
+                            GParamSpec   *pspec)
+{
+        SoupListener *listener = SOUP_LISTENER (object);
+        SoupListenerPrivate *priv = soup_listener_get_instance_private (listener);
+
+        switch (prop_id) {
+        case PROP_SOCKET:
+                priv->socket = g_value_dup_object (value);
+                break;
+        case PROP_TLS_CERTIFICATE:
+                priv->tls_certificate = g_value_dup_object (value);
+                break;
+        case PROP_TLS_DATABASE:
+                priv->tls_database = g_value_dup_object (value);
+                break;
+        case PROP_TLS_AUTH_MODE:
+                priv->tls_auth_mode = g_value_get_enum (value);
+                break;
+        default:
+                G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+                break;
+        }
+}
+
+static void
+soup_listener_get_property (GObject    *object,
+                            guint       prop_id,
+                            GValue     *value,
+                            GParamSpec *pspec)
+{
+        SoupListener *listener = SOUP_LISTENER (object);
+        SoupListenerPrivate *priv = soup_listener_get_instance_private (listener);
+
+        switch (prop_id) {
+        case PROP_SOCKET:
+                g_value_set_object (value, priv->socket);
+                break;
+        case PROP_TLS_CERTIFICATE:
+                g_value_set_object (value, priv->tls_certificate);
+                break;
+        case PROP_TLS_DATABASE:
+                g_value_set_object (value, priv->tls_database);
+                break;
+        case PROP_TLS_AUTH_MODE:
+                g_value_set_enum (value, priv->tls_auth_mode);
+                break;
+        default:
+                G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+                break;
+        }
+}
+
+static void
+soup_listener_class_init (SoupListenerClass *listener_class)
+{
+        GObjectClass *object_class = G_OBJECT_CLASS (listener_class);
+
+        object_class->constructed = soup_listener_constructed;
+        object_class->finalize = soup_listener_finalize;
+        object_class->set_property = soup_listener_set_property;
+        object_class->get_property = soup_listener_get_property;
+
+        /**
+         * SoupListener::new-connection:
+         * @listener: the listener
+         * @conn: the new connection
+         *
+         * Emitted when a listening socket receives a new connection.
+         *
+         * You must ref the @new if you want to keep it; otherwise it
+         * will be destroyed after the signal is emitted.
+         **/
+        signals[NEW_CONNECTION] =
+                g_signal_new ("new-connection",
+                              G_OBJECT_CLASS_TYPE (object_class),
+                              G_SIGNAL_RUN_FIRST,
+                              0,
+                              NULL, NULL,
+                              NULL,
+                              G_TYPE_NONE, 1,
+                              SOUP_TYPE_SERVER_CONNECTION);
+
+        /* properties */
+        properties[PROP_SOCKET] =
+                g_param_spec_object ("socket",
+                                     "Socket",
+                                     "The underlying GSocket",
+                                     G_TYPE_SOCKET,
+                                     G_PARAM_READWRITE | G_PARAM_CONSTRUCT_ONLY |
+                                     G_PARAM_STATIC_STRINGS);
+
+        properties[PROP_TLS_CERTIFICATE] =
+                g_param_spec_object ("tls-certificate",
+                                     "TLS Certificate",
+                                     "The server TLS certificate",
+                                     G_TYPE_TLS_CERTIFICATE,
+                                     G_PARAM_READWRITE |
+                                     G_PARAM_STATIC_STRINGS);
+
+        properties[PROP_TLS_DATABASE] =
+                g_param_spec_object ("tls-database",
+                                     "TLS Database",
+                                     "The server TLS database",
+                                     G_TYPE_TLS_DATABASE,
+                                     G_PARAM_READWRITE |
+                                     G_PARAM_STATIC_STRINGS);
+
+        properties[PROP_TLS_AUTH_MODE] =
+                g_param_spec_enum ("tls-auth-mode",
+                                   "TLS Authentication Mode",
+                                   "The server TLS authentication mode",
+                                   G_TYPE_TLS_AUTHENTICATION_MODE,
+                                   G_TLS_AUTHENTICATION_NONE,
+                                   G_PARAM_READWRITE |
+                                   G_PARAM_STATIC_STRINGS);
+
+        g_object_class_install_properties (object_class, LAST_PROPERTY, properties);
+}
+
+SoupListener *
+soup_listener_new (GSocket *socket,
+                   GError **error)
+{
+        int listening;
+
+        g_return_val_if_fail (G_IS_SOCKET (socket), NULL);
+        g_return_val_if_fail (error == NULL || *error == NULL, NULL);
+
+        if (!g_socket_get_option (socket, SOL_SOCKET, SO_ACCEPTCONN, &listening, error)) {
+                g_prefix_error (error, _("Could not import existing socket: "));
+                return NULL;
+        }
+
+        if (!listening && !g_socket_is_connected (socket)) {
+                g_set_error_literal (error, G_IO_ERROR, G_IO_ERROR_FAILED,
+                                     _("Can’t import unconnected socket"));
+                return NULL;
+        }
+
+        return g_object_new (SOUP_TYPE_LISTENER, "socket", socket, NULL);
+}
+
+SoupListener *
+soup_listener_new_for_address (GSocketAddress *address,
+                               GError        **error)
+{
+        GSocket *socket;
+        GSocketFamily family;
+        SoupListener *listener;
+
+        g_return_val_if_fail (G_IS_SOCKET_ADDRESS (address), NULL);
+        g_return_val_if_fail (error == NULL || *error == NULL, NULL);
+
+        family = g_socket_address_get_family (address);
+        socket = g_socket_new (family, G_SOCKET_TYPE_STREAM, G_SOCKET_PROTOCOL_DEFAULT, error);
+        if (!socket)
+                return NULL;
+
+        if (family == G_SOCKET_FAMILY_IPV6) {
+                GError *option_error = NULL;
+
+                g_socket_set_option (socket, IPPROTO_IPV6, IPV6_V6ONLY, TRUE, &option_error);
+                if (option_error) {
+                        g_warning ("Failed to set IPv6 only on socket: %s", option_error->message);
+                        g_error_free (option_error);
+                }
+        }
+
+        if (!g_socket_bind (socket, address, TRUE, error)) {
+                g_object_unref (socket);
+
+                return NULL;
+        }
+
+        if (!g_socket_listen (socket, error)) {
+                g_object_unref (socket);
+
+                return NULL;
+        }
+
+        listener = g_object_new (SOUP_TYPE_LISTENER, "socket", socket, NULL);
+        g_object_unref (socket);
+
+        return listener;
+}
+
+GSocket *
+soup_listener_get_socket (SoupListener *listener)
+{
+        SoupListenerPrivate *priv;
+
+        g_return_val_if_fail (SOUP_IS_LISTENER (listener), NULL);
+
+        priv = soup_listener_get_instance_private (listener);
+
+        return priv->socket;
+}
+
+void
+soup_listener_disconnect (SoupListener *listener)
+{
+        SoupListenerPrivate *priv;
+
+        g_return_if_fail (SOUP_IS_LISTENER (listener));
+
+        priv = soup_listener_get_instance_private (listener);
+        g_clear_object (&priv->socket);
+        if (priv->conn) {
+                g_io_stream_close (priv->conn, NULL, NULL);
+                g_clear_object (&priv->conn);
+        }
+}
+
+gboolean
+soup_listener_is_ssl (SoupListener *listener)
+{
+        SoupListenerPrivate *priv;
+
+        g_return_val_if_fail (SOUP_IS_LISTENER (listener), FALSE);
+
+        priv = soup_listener_get_instance_private (listener);
+
+        return priv->tls_certificate != NULL;
+}
+
+GInetSocketAddress *
+soup_listener_get_address (SoupListener *listener)
+{
+        SoupListenerPrivate *priv;
+
+        g_return_val_if_fail (SOUP_IS_LISTENER (listener), NULL);
+
+        priv = soup_listener_get_instance_private (listener);
+
+        if (!priv->local_addr) {
+                GError *error = NULL;
+
+                priv->local_addr = G_INET_SOCKET_ADDRESS (g_socket_get_local_address (priv->socket, &error));
+                if (priv->local_addr == NULL) {
+                        g_warning ("%s: %s", G_STRLOC, error->message);
+                        g_error_free (error);
+                        return NULL;
+                }
+        }
+
+        return priv->local_addr;
+}

libsoup/meson.build
@@ -45,12 +45,13 @@ soup_sources = [
   'server/soup-auth-domain.c',
   'server/soup-auth-domain-basic.c',
   'server/soup-auth-domain-digest.c',
+  'server/soup-listener.c',
   'server/soup-message-body.c',
   'server/soup-path-map.c',
   'server/soup-server.c',
+  'server/soup-server-connection.c',
   'server/soup-server-io.c',
   'server/soup-server-message.c',
-  'server/soup-socket.c',
 
   'websocket/soup-websocket.c',
   'websocket/soup-websocket-connection.c',

libsoup/server/soup-listener.h
@@ -1,0 +1,26 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 8 -*- */
+/*
+ * Copyright (C) 2022 Igalia S.L.
+ * Copyright (C) 2000-2003, Ximian, Inc.
+ */
+
+#pragma once
+
+#include "soup-types.h"
+
+G_BEGIN_DECLS
+
+#define SOUP_TYPE_LISTENER (soup_listener_get_type ())
+G_DECLARE_FINAL_TYPE (SoupListener, soup_listener, SOUP, LISTENER, GObject)
+
+SoupListener       *soup_listener_new              (GSocket        *socket,
+                                                    GError        **error);
+SoupListener       *soup_listener_new_for_address  (GSocketAddress *address,
+                                                    GError        **error);
+
+void                soup_listener_disconnect       (SoupListener   *listener);
+gboolean            soup_listener_is_ssl           (SoupListener   *listener);
+GSocket            *soup_listener_get_socket       (SoupListener   *listener);
+GInetSocketAddress *soup_listener_get_address      (SoupListener   *listener);
+
+G_END_DECLS

libsoup/server/soup-server-connection.h
@@ -1,0 +1,42 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 8 -*- */
+/*
+ * Copyright (C) 2022 Igalia S.L.
+ * Copyright (C) 2000-2003, Ximian, Inc.
+ */
+
+#pragma once
+
+#include "soup-types.h"
+#include <gio/gio.h>
+
+G_BEGIN_DECLS
+
+#define SOUP_TYPE_SERVER_CONNECTION (soup_server_connection_get_type ())
+G_DECLARE_FINAL_TYPE (SoupServerConnection, soup_server_connection, SOUP, SERVER_CONNECTION, GObject)
+
+SoupServerConnection *soup_server_connection_new                             (GSocket               *socket,
+                                                                              GTlsCertificate       *tls_certificate,
+                                                                              GTlsDatabase          *tls_database,
+                                                                              GTlsAuthenticationMode tls_auth_mode);
+SoupServerConnection *soup_server_connection_new_for_connection              (GIOStream             *connection,
+                                                                              GSocketAddress        *local_addr,
+                                                                              GSocketAddress        *remote_addr);
+void                  soup_server_connection_setup_async                     (SoupServerConnection  *conn,
+                                                                              GCancellable          *cancellable,
+                                                                              GAsyncReadyCallback    callback,
+                                                                              gpointer               user_data);
+gboolean              soup_server_connection_setup_finish                    (SoupServerConnection  *conn,
+                                                                              GAsyncResult          *result,
+                                                                              GError               **error);
+gboolean              soup_server_connection_is_ssl                          (SoupServerConnection  *conn);
+void                  soup_server_connection_disconnect                      (SoupServerConnection  *conn);
+gboolean              soup_server_connection_is_connected                    (SoupServerConnection  *conn);
+GSocket              *soup_server_connection_get_socket                      (SoupServerConnection  *conn);
+GSocket              *soup_server_connection_steal_socket                    (SoupServerConnection  *conn);
+GIOStream            *soup_server_connection_get_iostream                    (SoupServerConnection  *conn);
+GSocketAddress       *soup_server_connection_get_local_address               (SoupServerConnection  *conn);
+GSocketAddress       *soup_server_connection_get_remote_address              (SoupServerConnection  *conn);
+GTlsCertificate      *soup_server_connection_get_tls_peer_certificate        (SoupServerConnection  *conn);
+GTlsCertificateFlags  soup_server_connection_get_tls_peer_certificate_errors (SoupServerConnection  *conn);
+
+G_END_DECLS

libsoup/server/soup-server-io.c
@@ -18,7 +18,6 @@
 #include "soup-server-message-private.h"
 #include "soup-message-headers-private.h"
 #include "soup-misc.h"
-#include "soup-socket.h"
 
 struct _SoupServerMessageIOData {
         SoupMessageIOData base;
@@ -570,7 +569,7 @@ parse_headers (SoupServerMessage *msg,
 {
         char *req_method, *req_path, *url;
         SoupHTTPVersion version;
-	SoupSocket *sock;
+	SoupServerConnection *conn;
         const char *req_host;
         guint status;
         GUri *uri;
@@ -606,12 +605,12 @@ parse_headers (SoupServerMessage *msg,
                 return SOUP_STATUS_BAD_REQUEST;
         }
 
-	sock = soup_server_message_get_soup_socket (msg);
+	conn = soup_server_message_get_connection (msg);
 
 	if (!strcmp (req_path, "*") && req_host) {
 		/* Eg, "OPTIONS * HTTP/1.1" */
 		url = g_strdup_printf ("%s://%s/",
-				       soup_socket_is_ssl (sock) ? "https" : "http",
+				       soup_server_connection_is_ssl (conn) ? "https" : "http",
 				       req_host);
 		uri = g_uri_parse (url, SOUP_HTTP_URI_FLAGS, NULL);
                 soup_server_message_set_options_ping (msg, TRUE);
@@ -624,21 +623,21 @@ parse_headers (SoupServerMessage *msg,
 		uri = g_uri_parse (req_path, SOUP_HTTP_URI_FLAGS, NULL);
 	} else if (req_host) {
 		url = g_strdup_printf ("%s://%s%s",
-				       soup_socket_is_ssl (sock) ? "https" : "http",
+				       soup_server_connection_is_ssl (conn) ? "https" : "http",
 				       req_host, req_path);
 		uri = g_uri_parse (url, SOUP_HTTP_URI_FLAGS, NULL);
 		g_free (url);
 	} else if (soup_server_message_get_http_version (msg) == SOUP_HTTP_1_0) {
 		/* No Host header, no AbsoluteUri */
-		GInetSocketAddress *addr = soup_socket_get_local_address (sock);
+		GInetSocketAddress *addr = G_INET_SOCKET_ADDRESS (soup_server_connection_get_local_address (conn));
                 GInetAddress *inet_addr = g_inet_socket_address_get_address (addr);
                 char *local_ip = g_inet_address_to_string (inet_addr);
                 int port = g_inet_socket_address_get_port (addr);
                 if (port == 0)
                         port = -1;
 
-                uri = g_uri_build (SOUP_HTTP_URI_FLAGS, 
-                                   soup_socket_is_ssl (sock) ? "https" : "http",
+                uri = g_uri_build (SOUP_HTTP_URI_FLAGS,
+                                   soup_server_connection_is_ssl (conn) ? "https" : "http",
                                    NULL, local_ip, port, req_path, NULL, NULL);
 		g_free (local_ip);
 	} else
@@ -886,14 +885,14 @@ soup_server_message_read_request (SoupServerMessage        *msg,
 				  gpointer                  user_data)
 {
         SoupServerMessageIOData *io;
-	SoupSocket *sock;
+	SoupServerConnection *conn;
 
         io = g_slice_new0 (SoupServerMessageIOData);
         io->base.completion_cb = completion_cb;
         io->base.completion_data = user_data;
 
-	sock = soup_server_message_get_soup_socket (msg);
-        io->iostream = g_object_ref (soup_socket_get_iostream (sock));
+	conn = soup_server_message_get_connection (msg);
+        io->iostream = g_object_ref (soup_server_connection_get_iostream (conn));
         io->istream = g_io_stream_get_input_stream (io->iostream);
         io->ostream = g_io_stream_get_output_stream (io->iostream);
 

libsoup/server/soup-server-message-private.h
@@ -8,14 +8,14 @@
 #include "soup-server-message.h"
 #include "soup-auth-domain.h"
 #include "soup-message-io-data.h"
-#include "soup-socket.h"
+#include "soup-server-connection.h"
 
-SoupServerMessage *soup_server_message_new                 (SoupSocket               *sock);
+SoupServerMessage *soup_server_message_new                 (SoupServerConnection     *conn);
 void               soup_server_message_set_uri             (SoupServerMessage        *msg,
                                                             GUri                     *uri);
 void               soup_server_message_set_method          (SoupServerMessage        *msg,
                                                             const char               *method);
-SoupSocket        *soup_server_message_get_soup_socket     (SoupServerMessage        *msg);
+SoupServerConnection *soup_server_message_get_connection   (SoupServerMessage        *msg);
 void               soup_server_message_set_auth            (SoupServerMessage        *msg,
                                                             SoupAuthDomain           *domain,
                                                             char                     *user);

libsoup/server/soup-server-message.c
@@ -15,7 +15,6 @@
 #include "soup-connection.h"
 #include "soup-server-message-private.h"
 #include "soup-message-headers-private.h"
-#include "soup-socket.h"
 #include "soup-uri-utils-private.h"
 
 /**
@@ -38,7 +37,7 @@
 struct _SoupServerMessage {
         GObject             parent;
 
-        SoupSocket         *sock;
+        SoupServerConnection *conn;
         GSocket            *gsock;
         SoupAuthDomain     *auth_domain;
         char               *auth_user;
@@ -130,9 +129,9 @@ soup_server_message_finalize (GObject *object)
         g_clear_object (&msg->remote_addr);
         g_clear_object (&msg->local_addr);
 
-        if (msg->sock) {
-                g_signal_handlers_disconnect_by_data (msg->sock, msg);
-                g_object_unref (msg->sock);
+        if (msg->conn) {
+                g_signal_handlers_disconnect_by_data (msg->conn, msg);
+                g_object_unref (msg->conn);
         }
         g_clear_object (&msg->gsock);
         g_clear_pointer (&msg->remote_ip, g_free);
@@ -403,15 +402,15 @@ soup_server_message_class_init (SoupServerMessageClass *klass)
 }
 
 static void
-socket_disconnected (SoupServerMessage *msg)
+connection_disconnected (SoupServerMessage *msg)
 {
         g_signal_emit (msg, signals[DISCONNECTED], 0);
 }
 
 static gboolean
-socket_accept_certificate (SoupServerMessage    *msg,
-                           GTlsCertificate      *tls_certificate,
-                           GTlsCertificateFlags *tls_errors)
+connection_accept_certificate (SoupServerMessage    *msg,
+                               GTlsCertificate      *tls_certificate,
+                               GTlsCertificateFlags *tls_errors)
 {
 	gboolean accept = FALSE;
 
@@ -436,33 +435,33 @@ soup_server_message_set_tls_peer_certificate (SoupServerMessage   *msg,
 }
 
 static void
-re_emit_tls_certificate_changed (SoupServerMessage *msg,
-                                 GParamSpec        *pspec,
-                                 SoupSocket        *sock)
+re_emit_tls_certificate_changed (SoupServerMessage    *msg,
+                                 GParamSpec           *pspec,
+                                 SoupServerConnection *conn)
 {
         soup_server_message_set_tls_peer_certificate (msg,
-                                                      soup_socket_get_tls_certificate (sock),
-                                                      soup_socket_get_tls_certificate_errors (sock));
+                                                      soup_server_connection_get_tls_peer_certificate (conn),
+                                                      soup_server_connection_get_tls_peer_certificate_errors (conn));
 }
 
 SoupServerMessage *
-soup_server_message_new (SoupSocket *sock)
+soup_server_message_new (SoupServerConnection *conn)
 {
         SoupServerMessage *msg;
 
         msg = g_object_new (SOUP_TYPE_SERVER_MESSAGE, NULL);
-        msg->sock = g_object_ref (sock);
-        msg->gsock = soup_socket_get_gsocket (sock);
+        msg->conn = g_object_ref (conn);
+        msg->gsock = soup_server_connection_get_socket (conn);
         if (msg->gsock)
                 g_object_ref (msg->gsock);
 
-        g_signal_connect_object (sock, "disconnected",
-                                 G_CALLBACK (socket_disconnected),
+        g_signal_connect_object (conn, "disconnected",
+                                 G_CALLBACK (connection_disconnected),
                                  msg, G_CONNECT_SWAPPED);
-        g_signal_connect_object (sock, "accept-certificate",
-                                 G_CALLBACK (socket_accept_certificate),
+        g_signal_connect_object (conn, "accept-certificate",
+                                 G_CALLBACK (connection_accept_certificate),
                                  msg, G_CONNECT_SWAPPED);
-        g_signal_connect_object (sock, "notify::tls-certificate",
+        g_signal_connect_object (conn, "notify::tls-certificate",
                                  G_CALLBACK (re_emit_tls_certificate_changed),
                                  msg, G_CONNECT_SWAPPED);
 
@@ -478,10 +477,10 @@ soup_server_message_set_uri (SoupServerMessage *msg,
         msg->uri = soup_uri_copy_with_normalized_flags (uri);
 }
 
-SoupSocket *
-soup_server_message_get_soup_socket (SoupServerMessage *msg)
+SoupServerConnection *
+soup_server_message_get_connection (SoupServerMessage *msg)
 {
-        return msg->sock;
+        return msg->conn;
 }
 
 void
@@ -954,7 +953,7 @@ soup_server_message_get_remote_address (SoupServerMessage *msg)
 
         msg->remote_addr = msg->gsock ?
                 g_socket_get_remote_address (msg->gsock, NULL) :
-                G_SOCKET_ADDRESS (g_object_ref (soup_socket_get_remote_address (msg->sock)));
+                G_SOCKET_ADDRESS (g_object_ref (soup_server_connection_get_remote_address (msg->conn)));
 
         return msg->remote_addr;
 }
@@ -980,7 +979,7 @@ soup_server_message_get_local_address (SoupServerMessage *msg)
 
         msg->local_addr = msg->gsock ?
                 g_socket_get_local_address (msg->gsock, NULL) :
-                G_SOCKET_ADDRESS (g_object_ref (soup_socket_get_local_address (msg->sock)));
+                G_SOCKET_ADDRESS (g_object_ref (soup_server_connection_get_local_address (msg->conn)));
 
         return msg->local_addr;
 }
@@ -1013,7 +1012,7 @@ soup_server_message_get_remote_host (SoupServerMessage *msg)
                 iaddr = g_inet_socket_address_get_address (G_INET_SOCKET_ADDRESS (addr));
                 msg->remote_ip = g_inet_address_to_string (iaddr);
         } else {
-                GInetSocketAddress *addr = G_INET_SOCKET_ADDRESS (soup_socket_get_remote_address (msg->sock));
+                GInetSocketAddress *addr = G_INET_SOCKET_ADDRESS (soup_server_connection_get_remote_address (msg->conn));
                 GInetAddress *inet_addr = g_inet_socket_address_get_address (addr);
                 msg->remote_ip = g_inet_address_to_string (inet_addr);
         }
@@ -1050,13 +1049,13 @@ soup_server_message_steal_connection (SoupServerMessage *msg)
         stream = soup_server_message_io_steal (msg);
         if (stream) {
                 g_object_set_data_full (G_OBJECT (stream), "GSocket",
-                                        soup_socket_steal_gsocket (msg->sock),
+                                        soup_server_connection_steal_socket (msg->conn),
                                         g_object_unref);
         }
 
-        g_signal_handlers_disconnect_by_data (msg, msg->sock);
+        g_signal_handlers_disconnect_by_data (msg, msg->conn);
 
-        socket_disconnected (msg);
+        connection_disconnected (msg);
         g_object_unref (msg);
 
         return stream;

commit b545d5f047d01f086c88abddacff9ba519b5afa1

soup-listener: Plug a leak
Fixes: 25e96cad913cde650ca56ff76c3924fd97eb7125

libsoup/server/soup-listener.c
@@ -77,6 +77,8 @@ listen_watch (GObject      *pollable,
                 return G_SOURCE_REMOVE;
 
         conn = soup_server_connection_new (socket, priv->tls_certificate, priv->tls_database, priv->tls_auth_mode);
+        g_object_unref (socket);
+
         g_signal_emit (listener, signals[NEW_CONNECTION], 0, conn);
         g_object_unref (conn);
 