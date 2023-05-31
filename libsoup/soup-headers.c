GNOME/libsoup

commit 737eef099ca1e34d18245c54b6ed3ba54faf1f9c

Replace SoupURI with GUri

libsoup/soup-headers.c
@@ -696,7 +696,7 @@ decode_rfc5987 (char *encoded_string)
 	if (!q)
 		return FALSE;
 
-	decoded = soup_uri_decode (q + 1);
+	decoded = g_uri_unescape_string (q + 1, NULL);
 	if (iso_8859_1) {
 		char *utf8 =  g_convert_with_fallback (decoded, -1, "UTF-8",
 						       "iso-8859-1", "_",
@@ -910,7 +910,7 @@ append_param_rfc5987 (GString    *string,
 
 	g_string_append (string, name);
 	g_string_append (string, "*=UTF-8''");
-	encoded = soup_uri_encode (value, " *'%()<>@,;:\\\"/[]?=");
+	encoded = g_uri_escape_string (value, "*'%()<>@,;:\\\"/[]?=", FALSE);
 	g_string_append (string, encoded);
 	g_free (encoded);
 }

commit 883c4475b69150fc3ea28ffbe9ba53f7d667b1ce

headers: Fix encoding for UTF-8 params
This was a regression in 737eef09

Fixes: #349

libsoup/soup-headers.c
@@ -902,7 +902,7 @@ append_param_rfc5987 (GString    *string,
 
 	g_string_append (string, name);
 	g_string_append (string, "*=UTF-8''");
-	encoded = g_uri_escape_string (value, "*'%()<>@,;:\\\"/[]?=", FALSE);
+	encoded = g_uri_escape_string (value, "!#$&+-.^_`|~", FALSE);
 	g_string_append (string, encoded);
 	g_free (encoded);
 }

tests/header-parsing-test.c
@@ -1211,10 +1211,12 @@ struct {
 	{ "two", "test with spaces" },
 	{ "three", "test with \"quotes\" and \\s" },
 	{ "four", NULL },
-	{ "five", "test with \xC3\xA1\xC3\xA7\xC4\x89\xC3\xA8\xC3\xB1\xC5\xA3\xC5\xA1" }
+	{ "five", "test with \xC3\xA1\xC3\xA7\xC4\x89\xC3\xA8\xC3\xB1\xC5\xA3\xC5\xA1" },
+	{ "six", "あ,い!#$&+-.^_`|~" },
+	{ "seven", "あabc*'%123^_`|~" }
 };
 
-#define TEST_PARAMS_RESULT "one=foo, two=\"test with spaces\", three=\"test with \\\"quotes\\\" and \\\\s\", four, five*=UTF-8''test%20with%20%C3%A1%C3%A7%C4%89%C3%A8%C3%B1%C5%A3%C5%A1"
+#define TEST_PARAMS_RESULT "one=foo, two=\"test with spaces\", three=\"test with \\\"quotes\\\" and \\\\s\", four, five*=UTF-8''test%20with%20%C3%A1%C3%A7%C4%89%C3%A8%C3%B1%C5%A3%C5%A1, six*=UTF-8''%E3%81%82%2C%E3%81%84!#$&+-.^_`|~, seven*=UTF-8''%E3%81%82abc%2A%27%25123^_`|~"
 
 static void
 do_append_param_tests (void)