677c8f747c48cf101029ea776eb606432dae4215
Patrick Griffis <pgriffis@igalia.com>
2024-11-18 15:05:24 -0600

    fuzzing: More directly test various content types

--- fuzzing/fuzz_content_sniffer.c
+++ fuzzing/fuzz_content_sniffer.c
@@ -5,15 +5,33 @@ LLVMFuzzerTestOneInput (const unsigned char *data, size_t size)
 {
         fuzz_set_logging_func ();
 
+        /* Each content type has a different code path so lets test them all. */
+        static const char* content_types[] = {
+                NULL,
+                "application/unknown",
+                "text/plain",
+                "text/html",
+                "text/xml",
+                "image/something",
+                "video/something",
+        };
+
         GBytes *bytes = g_bytes_new (data, size);
         SoupContentSniffer *sniffer = soup_content_sniffer_new ();
-        SoupMessage *msg = soup_message_new (SOUP_METHOD_GET, "https://example.org");
-        char *content_type = soup_content_sniffer_sniff (sniffer, msg, bytes, NULL);
+
+        for (unsigned i = 0; i < G_N_ELEMENTS(content_types); i++) {
+                SoupMessage *msg = soup_message_new (SOUP_METHOD_GET, "https://example.org");
+                if (content_types[i])
+                        soup_message_headers_set_content_type (soup_message_get_response_headers(msg), content_types[i], NULL);
+
+                char *content_type = soup_content_sniffer_sniff (sniffer, msg, bytes, NULL);
+
+                g_object_unref (msg);
+                g_free (content_type);
+        }
 
         g_bytes_unref (bytes);
         g_object_unref (sniffer);
-        g_object_unref (msg);
-        g_free (content_type);
 
         return 0;
 }
